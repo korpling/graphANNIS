387c156fdaa3895baf1fedd09887eaaf9bbd4c56
Thomas Krause <thomaskrause@posteo.de>
2014-08-05 10:44:12 +0200

    activate C++ 11 for gcc

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-
+#include <stx/btree_map>
+#include <cstdint>
 
 using namespace std;
 
 int main()
 {
-  cout << "Hello World!" << endl;
+  stx::btree_map<uint32_t,uint32_t> testmap;
+  testmap[234] = 23;
+  cout << "Hello World! " << testmap[234] << endl;
   return 0;
 }
 
