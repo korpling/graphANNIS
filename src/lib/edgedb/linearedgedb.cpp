947ee9c45b8ce534b5321b3a2b0d885026d97d17
Thomas Krause <thomaskrause@posteo.de>
2014-09-20 14:47:17 +0200

    added empty implementation for linear graphs

--- src/lib/edgedb/linearedgedb.cpp
+++ src/lib/edgedb/linearedgedb.cpp
@@ -1,5 +1,69 @@
 #include "linearedgedb.h"
 
+using namespace annis;
+using namespace std;
+
 LinearEdgeDB::LinearEdgeDB()
 {
+}
+
+void LinearEdgeDB::addEdge(const Edge &edge)
+{
+  throw("Not implemented yet");
+}
+
+void LinearEdgeDB::addEdgeAnnotation(const Edge &edge, const Annotation &anno)
+{
+  throw("Not implemented yet");
+}
+
+void LinearEdgeDB::clear()
+{
+  node2pos.clear();
+  pos2node.clear();
+}
+
+bool LinearEdgeDB::isConnected(const Edge &edge, unsigned int minDistance, unsigned int maxDistance) const
+{
+  throw("Not implemented yet");
+}
+
+EdgeIterator *LinearEdgeDB::findConnected(nodeid_t sourceNode, unsigned int minDistance, unsigned int maxDistance) const
+{
+  throw("Not implemented yet");
+}
+
+std::vector<Annotation> LinearEdgeDB::getEdgeAnnotations(const Edge &edge) const
+{
+  throw("Not implemented yet");
+}
+
+std::vector<nodeid_t> LinearEdgeDB::getOutgoingEdges(nodeid_t sourceNode) const
+{
+  throw("Not implemented yet");
+}
+
+std::uint32_t LinearEdgeDB::numberOfEdgeAnnotations() const
+{
+  throw("Not implemented yet");
+}
+
+std::uint32_t LinearEdgeDB::numberOfEdges() const
+{
+  throw("Not implemented yet");
+}
+
+bool LinearEdgeDB::save(string dirPath)
+{
+  throw("Not implemented yet");
+}
+
+bool LinearEdgeDB::load(string dirPath)
+{
+  throw("Not implemented yet");
+}
+
+LinearEdgeDB::~LinearEdgeDB()
+{
+
 }
--- src/lib/edgedb/linearedgedb.h
+++ src/lib/edgedb/linearedgedb.h
@@ -1,10 +1,42 @@
 #ifndef LINEAREDGEDB_H
 #define LINEAREDGEDB_H
 
-class LinearEdgeDB
+#include <stx/btree_map>
+#include "../edgedb.h"
+
+namespace annis
+{
+class LinearEdgeDB : public EdgeDB
 {
 public:
   LinearEdgeDB();
+
+  virtual void addEdge(const Edge& edge);
+  virtual void addEdgeAnnotation(const Edge& edge, const Annotation& anno);
+  virtual void clear();
+
+  virtual bool isConnected(const Edge& edge, unsigned int minDistance, unsigned int maxDistance) const;
+  virtual EdgeIterator* findConnected(
+                                           nodeid_t sourceNode,
+                                           unsigned int minDistance,
+                                           unsigned int maxDistance) const;
+
+  virtual std::vector<Annotation> getEdgeAnnotations(const Edge& edge) const;
+  virtual std::vector<nodeid_t> getOutgoingEdges(nodeid_t sourceNode) const;
+
+  virtual bool load(std::string dirPath) = 0;
+  virtual bool save(std::string dirPath) = 0;
+
+  virtual std::uint32_t numberOfEdges() const = 0;
+  virtual std::uint32_t numberOfEdgeAnnotations() const = 0;
+
+  virtual ~LinearEdgeDB();
+
+private:
+  stx::btree_map<nodeid_t, std::uint32_t> node2pos;
+  stx::btree_map<std::uint32_t, nodeid_t> pos2node;
 };
+
+} // end namespace annis
 
 #endif // LINEAREDGEDB_H
