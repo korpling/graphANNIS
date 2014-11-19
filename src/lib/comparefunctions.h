8bd00f8a2056d62985d0c4b649afecd17b499de7
Thomas Krause <thomaskrause@posteo.de>
2014-11-19 10:19:25 +0100

    typo: wrong operator

--- src/lib/comparefunctions.h
+++ src/lib/comparefunctions.h
@@ -74,7 +74,7 @@ static bool checkAnnotationEqual(const struct Annotation &a, const struct Annota
   }
 
   // if equal, compare by namespace (non lexical but just by the ID)
-  if(a.ns != 0 & b.ns != 0 && a.ns != b.ns)
+  if(a.ns != 0 && b.ns != 0 && a.ns != b.ns)
   {
     return false;
   }
