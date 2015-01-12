988c6e7096b34822abd989497680beb5d0fe293c
Thomas Krause <thomaskrause@posteo.de>
2015-01-12 15:17:11 +0100

    implement TuebaDZ test

--- tests/SearchTestTueBaDZ.h
+++ tests/SearchTestTueBaDZ.h
@@ -7,6 +7,8 @@
 #include "operators/precedence.h"
 #include "operators/overlap.h"
 #include "operators/inclusion.h"
+#include "operators/pointing.h"
+#include "operators/dominance.h"
 #include "annotationsearch.h"
 #include "query.h"
 
@@ -50,7 +52,8 @@ class SearchTestTueBaDZ : public ::testing::Test {
 
 /*
  * Query:
- * node & merged:pos="PPER" & node & mmax:relation="anaphoric" & node & node & mmax:relation="anaphoric"
+
+node & merged:pos="PPER" & node & mmax:relation="anaphoric" & node & node & mmax:relation="anaphoric"
 & #1 >[func="ON"] #3
 & #3 >* #2
 & #2 _i_ #4
@@ -58,24 +61,35 @@ class SearchTestTueBaDZ : public ::testing::Test {
 & #6 >* #7
 & #4 ->anaphoric #7
 */
-TEST_F(SearchTestTueBaDZ, DISABLED_Benchmark1) {
+TEST_F(SearchTestTueBaDZ, Benchmark1) {
 
   Query q(db);
-  q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
-  q.addNode(std::make_shared<AnnotationNameSearch>(db, "merged", "pos", "PPER"));
-  q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
-  q.addNode(std::make_shared<AnnotationNameSearch>(db, "mmax", "relation", "anaphoric"));
-  q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
-  q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
-  q.addNode(std::make_shared<AnnotationNameSearch>(db, "mmax", "relation", "anaphoric"));
+  auto n1 = q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
+  auto n2 = q.addNode(std::make_shared<AnnotationNameSearch>(db, "merged", "pos", "PPER"));
+  auto n3 = q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
+  auto n4 = q.addNode(std::make_shared<AnnotationNameSearch>(db, "mmax", "relation", "anaphoric"));
+  auto n5 = q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
+  auto n6 = q.addNode(std::make_shared<AnnotationNameSearch>(db, annis_ns, annis_node_name));
+  auto n7 = q.addNode(std::make_shared<AnnotationNameSearch>(db, "mmax", "relation", "anaphoric"));
 
-  const EdgeDB* edbAnaphoric = db.getEdgeDB(ComponentType::POINTING, "mmax", "anaphoric");
+  Annotation funcOnAnno =
+      Init::initAnnotation(db.strings.add("func"), db.strings.add("ON"));
 
-  q.addOperator(std::make_shared<Inclusion>(db), 1, 3);
+  q.addOperator(std::make_shared<Inclusion>(db), n2, n4);
+  q.addOperator(std::make_shared<Pointing>(db, "", "anaphoric"), n4, n7);
+  q.addOperator(std::make_shared<Dominance>(db, "", "", funcOnAnno), n1, n3);
+  q.addOperator(std::make_shared<Dominance>(db, "", "", 1, uintmax), n3, n2);
+  q.addOperator(std::make_shared<Dominance>(db, "", "", funcOnAnno), n5, n6);
+  q.addOperator(std::make_shared<Dominance>(db, "", "", 1, uintmax), n6, n7);
 
   unsigned int counter=0;
+  while(q.hasNext() && counter < 10u)
+  {
+    q.next();
+    counter++;
+  }
 
-  EXPECT_EQ(373436u, counter);
+  EXPECT_EQ(0u, counter);
 }
 
 
