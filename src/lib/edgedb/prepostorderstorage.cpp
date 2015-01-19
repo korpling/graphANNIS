1dafd9501c95adc8e12cece011f1cc17ee655da0
Thomas Krause <thomaskrause@posteo.de>
2015-01-19 14:42:26 +0100

    initial code for creating a pre/post order as edge storage

--- src/lib/edgedb/prepostorderstorage.cpp
+++ src/lib/edgedb/prepostorderstorage.cpp
@@ -1,5 +1,14 @@
 #include "prepostorderstorage.h"
 
+#include <set>
+#include <stack>
+
+#include <fstream>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/map.hpp>
+
+
 using namespace annis;
 
 PrePostOrderStorage::PrePostOrderStorage(StringStorage &strings, const Component &component)
@@ -13,8 +22,111 @@ PrePostOrderStorage::~PrePostOrderStorage()
 
 }
 
+bool PrePostOrderStorage::load(std::string dirPath)
+{
+  node2order.clear();
+  order2node.clear();
+
+  bool result = FallbackEdgeDB::load(dirPath);
+  std::ifstream in;
+
+  in.open(dirPath + "/node2order.btree", std::ios::binary);
+  result = result && node2order.restore(in);
+  in.close();
+
+  in.open(dirPath + "/order2node.btree", std::ios::binary);
+  result = result && order2node.restore(in);
+  in.close();
+
+  return result;
+}
+
+bool PrePostOrderStorage::save(std::string dirPath)
+{
+  bool result = FallbackEdgeDB::save(dirPath);
+
+  std::ofstream out;
+
+  out.open(dirPath + "/node2order.btree", std::ios::binary);
+  node2order.dump(out);
+  out.close();
+
+  out.open(dirPath + "/order2node.btree", std::ios::binary);
+  order2node.dump(out);
+  out.close();
+
+  return result;
+
+}
+
 void PrePostOrderStorage::calculateIndex()
 {
+  using ItType = stx::btree_set<Edge>::const_iterator;
+  node2order.clear();
+  order2node.clear();
 
+  // find all roots of the component
+  std::set<nodeid_t> roots;
+  // first add all nodes that are a source of an edge as possible roots
+  for(ItType it = getEdgesBegin(); it != getEdgesEnd(); it++)
+  {
+    roots.insert(it->source);
+  }
+  // second delete the ones that have an outgoing edge
+  for(ItType it = getEdgesBegin(); it != getEdgesEnd(); it++)
+  {
+    roots.erase(it->target);
+  }
+
+  // traverse the graph for each sub-component
+  for(const auto& startNode : roots)
+  {
+    unsigned int lastDistance = 0;
+
+    uint32_t currentOrder = 0;
+    std::stack<nodeid_t> nodeStack;
+
+    order2node[currentOrder] = startNode;
+    node2order[startNode].pre = currentOrder++;
+    nodeStack.push(startNode);
+
+    FallbackDFSIterator dfs(*this, startNode, 1, uintmax);
+    for(DFSIteratorResult step = dfs.nextDFS(); step.found;
+          step = dfs.nextDFS())
+    {
+      if(step.distance > lastDistance)
+      {
+        // first visited, set pre-order
+        order2node[currentOrder] = step.node;
+        node2order[step.node].pre = currentOrder++;
+        nodeStack.push(step.node);
+      }
+      else if(step.distance == lastDistance)
+      {
+        // neighbour node, the last subtree was iterated completly, thus the last node
+        // can be assigned a post-order
+        order2node[currentOrder] = nodeStack.top();
+        node2order[nodeStack.top()].post = currentOrder++;
+        nodeStack.pop();
+
+        // new node
+        order2node[currentOrder] = step.node;
+        node2order[step.node].pre = currentOrder++;
+        nodeStack.push(step.node);
+
+      }
+      else
+      {
+        // parent node, the subtree was iterated completly, thus the last node
+        // can be assigned a post-order
+        order2node[currentOrder] = nodeStack.top();
+        node2order[nodeStack.top()].post = currentOrder++;
+        nodeStack.pop();
+
+        // the current node was already visited
+      }
+      lastDistance = step.distance;
+    } // end for each DFS step
+  } // end for each root
 }
 
--- src/lib/edgedb/prepostorderstorage.h
+++ src/lib/edgedb/prepostorderstorage.h
@@ -3,8 +3,17 @@
 
 #include "fallbackedgedb.h"
 
+#include <stx/btree_map>
+
 namespace annis
 {
+
+struct PrePost
+{
+  uint32_t pre;
+  uint32_t post;
+};
+
 
 class PrePostOrderStorage : public FallbackEdgeDB
 {
@@ -12,13 +21,37 @@ public:
   PrePostOrderStorage(StringStorage& strings, const Component& component);
   virtual ~PrePostOrderStorage();
 
+  virtual bool load(std::string dirPath);
+  virtual bool save(std::string dirPath);
+
   virtual void calculateIndex();
 
 private:
-
+  stx::btree_map<nodeid_t, PrePost> node2order;
+  stx::btree_map<uint32_t, uint32_t> order2node;
 
 };
 
 } // end namespace annis
+
+namespace std
+{
+template<>
+struct less<annis::PrePost>
+{
+  bool operator()(const struct annis::PrePost &a, const struct annis::PrePost &b) const
+  {
+    // compare by pre-order
+    if(a.pre < b.pre) {return true;} else if(a.pre > b.pre) {return false;}
+
+    // compare by post-order
+    if(a.post < b.post) {return true;} else if(a.post > b.post) {return false;}
+
+    // they are equal
+    return false;
+  }
+};
+
+} // end namespace std
 
 #endif // PREPOSTORDERSTORAGE_H
