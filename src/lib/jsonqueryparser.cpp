3e9ffeac4a99bfdaf37a66c39c5fadc0c2e7ca69
Thomas Krause <thomaskrause@posteo.de>
2016-01-03 16:14:03 +0100

    add empty jsonqueryparser

---
+++ src/lib/jsonqueryparser.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   JSONQueryParser.cpp
+ * Author: Thomas Krause <thomaskrause@posteo.de>
+ * 
+ * Created on 3. Januar 2016, 16:05
+ */
+
+#include "jsonqueryparser.h"
+
+using namespace annis;
+
+JSONQueryParser::JSONQueryParser() {
+}
+
+Query JSONQueryParser::parse(const std::string& json) {
+  Query q;
+  
+  return q;
+}
+
+JSONQueryParser::~JSONQueryParser() {
+}
+
---
+++ src/lib/jsonqueryparser.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   JSONQueryParser.h
+ * Author: Thomas Krause <thomaskrause@posteo.de>
+ *
+ * Created on 3. Januar 2016, 16:05
+ */
+
+#ifndef JSONQUERYPARSER_H
+#define JSONQUERYPARSER_H
+
+#include "query.h"
+
+namespace annis {
+
+  class JSONQueryParser {
+  public:
+    JSONQueryParser();
+    JSONQueryParser(const JSONQueryParser& orig) = delete;
+    JSONQueryParser &operator=(const JSONQueryParser&) = delete;
+
+    Query parse(const std::string& json);
+
+    virtual ~JSONQueryParser();
+  private:
+
+  };
+
+}
+
+#endif /* JSONQUERYPARSER_H */
+
