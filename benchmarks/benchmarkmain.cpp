c01531c9bee3260ef74b2331bf1e81e04a34f3ca
Thomas Krause <thomaskrause@posteo.de>
2016-01-04 13:26:02 +0100

    add class that will enable benchmark of a directory of json files

--- benchmarks/benchmarkmain.cpp
+++ benchmarks/benchmarkmain.cpp
@@ -1,15 +1,19 @@
 #include <celero/Celero.h>
 #include <humblelogging/api.h>
 
-HUMBLE_LOGGER(logger, "default");
-
+#include "dynamicbenchmark.h"
 
 int main(int argc, char **argv)
 {
 
   humble::logging::Factory &fac = humble::logging::Factory::getInstance();
+  fac.setDefaultLogLevel(humble::logging::LogLevel::All);
+  fac.setDefaultFormatter(new humble::logging::PatternFormatter("[%lls] %m\n"));
+  fac.registerAppender(new humble::logging::ConsoleAppender());
 
-
+  // test stuff
+  //annis::DynamicBenchmark dynamicTest("data", "queries/Benchmark_ridges", "ridges");
+  
   fac.setDefaultLogLevel(humble::logging::LogLevel::Info);
   fac.setDefaultFormatter(new humble::logging::PatternFormatter("[%date]- %m (%lls, %filename:%line)\n"));
   fac.registerAppender(new humble::logging::FileAppender("benchmark_annis4.log"));
---
+++ benchmarks/dynamicbenchmark.h
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   DynamicBenchmark.h
+ * Author: thomas
+ *
+ * Created on 4. Januar 2016, 11:54
+ */
+
+#ifndef DYNAMICBENCHMARK_H
+#define DYNAMICBENCHMARK_H
+
+#include <celero/Celero.h>
+#include <boost/filesystem.hpp>
+
+namespace annis
+{
+
+class DynamicBenchmark {
+public:
+  DynamicBenchmark(std::string dataDir, std::string queriesDir, std::string corpus);
+  DynamicBenchmark(const DynamicBenchmark& orig)  = delete;
+  virtual ~DynamicBenchmark();
+private:
+  std::string dataDir;
+  std::string queriesDir;
+  std::string corpus;
+  
+  void addBenchmark(const boost::filesystem::path& path);
+};
+} // end namespace annis
+#endif /* DYNAMICBENCHMARK_H */
+
