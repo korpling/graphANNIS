9da4cf7221366360fd90178f091f162bbe3471e7
Thomas Krause <thomaskrause@posteo.de>
2016-01-04 14:22:33 +0100

    move implementation to cpp file again

--- benchmarks/dynamicbenchmark.cpp
+++ benchmarks/dynamicbenchmark.cpp
@@ -7,5 +7,31 @@
 
 #include "dynamicbenchmark.h"
 
+
+#include <humblelogging/api.h>
+#include <boost/filesystem.hpp>
+#include <boost/format.hpp>
+
 using namespace annis;
 
+
+DynamicBenchmark::DynamicBenchmark(std::string dataDir, std::string queriesDir, std::string corpusName)
+: dataDir(dataDir), queriesDir(queriesDir), corpus(corpusName) {
+
+  // find all file ending with ".json" in the folder
+  boost::filesystem::directory_iterator fileEndIt;
+
+  boost::filesystem::directory_iterator itFiles(queriesDir);
+  while (itFiles != fileEndIt) {
+    const auto filePath = itFiles->path();
+    if (filePath.extension().string() == ".json") {
+      addBenchmark(filePath);
+    }
+    itFiles++;
+  }
+}
+
+void DynamicBenchmark::addBenchmark(const boost::filesystem::path& path) {
+  HL_INFO(logger, (boost::format("adding benchmark %1%") % path.string()).str());
+  // create fallback fixture
+}
--- benchmarks/dynamicbenchmark.h
+++ benchmarks/dynamicbenchmark.h
@@ -15,45 +15,22 @@
 #define DYNAMICBENCHMARK_H
 
 #include "benchmark.h"
-
-#include <humblelogging/api.h>
-#include <celero/Celero.h>
 #include <boost/filesystem.hpp>
-#include <boost/format.hpp>
 
 namespace annis {
   class DynamicBenchmark {
   public:
 
-    DynamicBenchmark(std::string dataDir, std::string queriesDir, std::string corpusName)
-    : dataDir(dataDir), queriesDir(queriesDir), corpus(corpusName) {
-
-      // find all file ending with ".json" in the folder
-      boost::filesystem::directory_iterator fileEndIt;
-
-      boost::filesystem::directory_iterator itFiles(queriesDir);
-      while (itFiles != fileEndIt) {
-        const auto filePath = itFiles->path();
-        if (filePath.extension().string() == ".json") {
-          addBenchmark(filePath);
-        }
-        itFiles++;
-      }
-    }
+    DynamicBenchmark(std::string dataDir, std::string queriesDir, std::string corpusName);
 
     DynamicBenchmark(const DynamicBenchmark& orig) = delete;
     virtual ~DynamicBenchmark() {}
   private:
-    humble::logging::Logger& logger = humble::logging::Factory::getInstance().getLogger("default");
     std::string dataDir;
     std::string queriesDir;
     std::string corpus;
 
-    void addBenchmark(const boost::filesystem::path& path)
-    {
-      HL_INFO(logger, (boost::format("adding benchmark %1%") % path.string()).str());
-      // create fallback fixture
-    }
+    void addBenchmark(const boost::filesystem::path& path);
   };
 } // end namespace annis
 #endif /* DYNAMICBENCHMARK_H */
