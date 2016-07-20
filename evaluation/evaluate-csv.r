7951380c3ae247d4fced1c32ec659f6c199d8da5
Thomas Krause <thomaskrause@posteo.de>
2016-07-20 12:06:42 +0200

    Change R script to function that collects the values.

--- evaluation/evaluate-csv.r
+++ evaluation/evaluate-csv.r
@@ -1,14 +1,22 @@
-rm(list = ls())
-fn <- c("2016-07-19.csv")
+evaluate_benchmark <- function(fn) {
+  
+  d = read.csv(fn)
+  d <- subset(d, Experiment=='Optimized')
+  
+  worse <- subset(d, Baseline >= 1.0)
+  better <- subset(d, Baseline < 1.0)
+  
+  h <- sort(d$Baseline)
+  #barplot(h, main=paste("speedup distribution for ", fn), log="y")
+  q <- quantile(d$Baseline)
+  
+  return (list(worse=nrow(worse), better=nrow(better), quantile=q, dist=h))
+  
+}
 
-d = read.csv(fn)
-options(scipen = 5)
-d <- subset(d, Experiment=='Optimized')
 
-worse <- subset(d, Baseline >= 1.0)
-better <- subset(d, Baseline < 1.0)
 
-barplot(sort(d$Baseline), main="Baseline speedup distribution", log="y")
-message("worse: ", nrow(worse), " better: ", nrow(better))
-quantile(d$Baseline)
+
+
+
 
