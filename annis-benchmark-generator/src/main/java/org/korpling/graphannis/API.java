18abb5355390e83f3db939c32094d4051b2b911b
Thomas Krause <thomaskrause@posteo.de>
2016-07-25 22:17:52 +0200

    Proper linking of needed libraries and allocation of native object.

--- annis-benchmark-generator/src/main/java/org/korpling/graphannis/API.java
+++ annis-benchmark-generator/src/main/java/org/korpling/graphannis/API.java
@@ -10,9 +10,7 @@ import org.korpling.annis.benchmark.generator.QueryToJSON;
 import annis.ql.parser.AnnisParserAntlr;
 import annis.ql.parser.QueryData;
 
-@Platform(include="annis/api.h", link={"ANNIS4"}, 
-	preload={"boost_system", "boost_filesystem", "boost_serialization"},
-	preloadpath="/usr/lib/")
+@Platform(include="annis/api.h", link={"boost_system", "boost_filesystem", "boost_serialization", "humblelogging", "ANNIS4"})
 @Namespace("annis")
 public class API extends Pointer {
 
@@ -20,16 +18,24 @@ public class API extends Pointer {
 		Loader.load();
 	}
 	
+	public API()
+	{
+	  allocate();
+	}
+	
+	public native void allocate(); 
+	
 	public native long count(@StdString String corpus, String queryAsJSON);
 	
 	
 	public static void main(String[] args) {
-		API test = new API();
+		
+	  API test = new API();
 		
 		AnnisParserAntlr parser = new AnnisParserAntlr();
 		
 		QueryData queryData = parser.parse(args[0], null);
-		System.out.println("Count: " + test.count("pcc", QueryToJSON.serializeQuery(queryData)));
+		System.out.println("Count: " + test.count("pcc2", QueryToJSON.serializeQuery(queryData)));
 		
 		test.close();
     }
