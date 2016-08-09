6e6138c05a78b1b68071099f2aad713357e988b8
Thomas Krause <thomaskrause@posteo.de>
2016-08-09 15:52:35 +0200

    Include humblelogging via ExternalProject cmake mechanism.

---
+++ cmake/humblelogging.cmake
@@ -0,0 +1,29 @@
+##################
+# Humble logging #
+##################
+
+ExternalProject_Add(
+  HumbleLogging
+
+  GIT_REPOSITORY "https://github.com/mfreiholz/humble-logging-library.git"
+  GIT_TAG "v3.0.1"
+
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ""
+
+  SOURCE_DIR "${CMAKE_SOURCE_DIR}/ext/humblelogging"
+  CMAKE_ARGS -DBuildShared=OFF -DBuildExamples=OFF -DCMAKE_INSTALL_PREFIX=${GLOBAL_OUTPUT_PATH}/humblelogging
+
+  TEST_COMMAND ""
+)
+
+ExternalProject_Add_Step(
+  HumbleLogging CopyToBin
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${GLOBAL_OUTPUT_PATH}/humblelogging/bin ${GLOBAL_OUTPUT_PATH}
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${GLOBAL_OUTPUT_PATH}/humblelogging/lib ${GLOBAL_OUTPUT_PATH}
+  DEPENDEES install
+)
+
+set(HumbleLogging_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/ext/humblelogging/include")
+set(HumbleLogging_LIBRARIES "${CMAKE_SHARED_LIBRARY_PREFIX}humblelogging${CMAKE_SHARED_LIBRARY_SUFFIX}")
+include_directories(${HumbleLogging_INCLUDE_DIRS})
