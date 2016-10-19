79bb398a79a59e00cdd7ded26b36db6796b4233a
Thomas Krause <thomaskrause@posteo.de>
2016-10-19 13:36:08 +0200

    Add test main class in graphANNIS.

---
+++ graphannis-api/src/main/java/org/corpus_tools/graphannis/Main.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 Thomas Krause.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.corpus_tools.graphannis;
+
+/**
+ *
+ * @author thomas
+ */
+public class Main
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    if(args.length > 0)
+    {
+      API.Search search = new API.Search(args[0]);
+      
+    }
+    else
+    {
+      System.err.println("You have to a give a database directory as argument.");
+    }
+  }
+  
+}
