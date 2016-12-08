aeb044c9e60bb4749c6b37f9ede425d82534ddfd
Thomas Krause <thomaskrause@posteo.de>
2016-12-08 16:58:53 +0100

    Use a deque in the ListWrapper for faster resets.

--- src/lib/annis/wrapper.h
+++ src/lib/annis/wrapper.h
@@ -24,12 +24,12 @@ namespace annis
 
     void addMatch(const Match& m)
     {
-      orig.push(m);
+      orig.push_back(m);
     }
 
     void addMatch(const nodeid_t& m)
     {
-      orig.push({m,
+      orig.push_back({m,
         {0, 0, 0}});
     }
 
@@ -41,18 +41,15 @@ namespace annis
       }
       else
       {
-        result = orig.front();
-        orig.pop();
+        result = std::move(orig.front());
+        orig.pop_front();
         return true;
       }
     }
 
     virtual void reset() override
     {
-      while (!orig.empty())
-      {
-        orig.pop();
-      }
+      orig.clear();
     }
 
     virtual ~ListWrapper();
@@ -65,7 +62,7 @@ namespace annis
     }
 
   private:
-    std::queue<Match, std::list<Match> > orig;
+    std::deque<Match > orig;
   };
 
   class JoinWrapIterator : public ListWrapper
