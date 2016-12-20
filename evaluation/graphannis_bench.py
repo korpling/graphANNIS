f51308109ad186f3da6d7da1b514b75a105b6fc4
Thomas Krause <thomaskrause@posteo.de>
2016-12-20 19:17:52 +0100

    Rename columns in evaluation script to be more python like.

--- evaluation/graphannis_bench.py
+++ evaluation/graphannis_bench.py
@@ -8,12 +8,13 @@ Created on Wed Dec 14 08:53:14 2016
 import pandas as pd
 import matplotlib.pyplot as plt
 import collections
+import numpy as np
 
 
 def getaql(x, querydir):
     aqlFileName = ""
-    group = x['Group']
-    problemSpace = x['Problem Space']
+    group = x.Group
+    problemSpace = x.queryID
 
     aqlFileName = querydir + "/" + group + "/" + str.format("{:05d}", problemSpace) + ".aql"
 
@@ -24,6 +25,12 @@ def getaql(x, querydir):
 
 def extract(fn, querydir=None):
     data = pd.read_csv(fn, delim_whitespace=False)
+    
+    # rename columns that have python incompatible names
+    data.rename(columns={"us/Iteration": "time", "Problem Space" : "queryID"}, inplace=True)
+    
+    # don't use microseconds but milliseconds
+    data.time = data.time / 1000.0
 
     if querydir is not None:
         # try to get the original AQL queries
@@ -37,18 +44,18 @@ def desc(d):
 
     Desc = collections.namedtuple("Desc", ["worse", "better", "quantile", "sumTime"])    
     
-    worse = len(d.loc[d["Baseline"] >= 1.0])
-    better = len(d.loc[d["Baseline"] < 1.0])
+    worse = len(d.loc[d.Baseline >= 1.0])
+    better = len(d.loc[d.Baseline < 1.0])
     
-    q = d['Baseline'].quantile([.0, .25, .5, .75, 1.0])    
+    q = d.Baseline.quantile([.1, .25, .5, .75, 1.0])    
 
-    sumTime = d['us/Iteration'].sum()    
+    sumTime = d.time.sum()    
     
     return Desc(worse, better, q, sumTime)
     
 def plot(d, header=None):
 
-    h = (1.0 / d['Baseline']).sort_values().to_frame()    
+    h = (1.0 / d.Baseline).sort_values().to_frame()    
     h["index1"] = range(len(h))
     h.columns = ["speedup", "aql"]
 
