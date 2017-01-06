ecb552a2cc3c07c9244102025bef8bf971f1b480
Thomas Krause <thomaskrause@posteo.de>
2017-01-06 15:07:40 +0100

    Updating paths and versions.

--- graphannis-api/src/main/java/org/corpus_tools/graphannis/info/AnnisApiInfo.java
+++ graphannis-api/src/main/java/org/corpus_tools/graphannis/info/AnnisApiInfo.java
@@ -17,11 +17,10 @@ import org.bytedeco.javacpp.tools.InfoMapper;
         link={"re2" ,"boost_system", "boost_filesystem", "boost_thread", "humblelogging", "annis"}
         ),
         @Platform(value = "windows",
-		  link={"re2", "boost_system-vc140-mt-1_61", "boost_filesystem-vc140-mt-1_61", "boost_thread-vc140-mt-1_61" , "humblelogging", "annis"}
+		  link={"re2", "humblelogging", "annis"}
 		)})
 public class AnnisApiInfo implements InfoMapper
 {
-
   @Override
   public void map(InfoMap infoMap)
   {
