52c7862b23354ad5e3a8164f4dada25a61d61b35
Thomas Krause <thomaskrause@posteo.de>
2017-08-12 19:57:14 +0200

    Add a simple initial StringStorage implementation in Rust

---
+++ graphannis-base/src/annis/stringstorage.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+use std::collections::BTreeMap;
+
+pub struct StringStorage {
+    by_id: HashMap<u32,String>,
+    by_value: BTreeMap<String, u32>
+}
+
+impl StringStorage {
+
+    pub fn new() -> StringStorage {
+        StringStorage {
+            by_id: HashMap::new(),
+            by_value: BTreeMap::new()
+        }
+    }
+
+    pub fn str(&self, id: u32) -> Option<&String> {
+        return self.by_id.get(&id);
+    }
+
+    pub fn add(&mut self, val: String) -> u32 {
+        {
+            let existing = self.by_value.get(&val);
+            if existing.is_some() {
+                return *(existing.unwrap());
+            }
+        }
+        // non-existing: add a new value
+        let mut id = self.by_id.len() as u32 + 1; // since 0 is taken as ANY value begin with 1
+        while self.by_id.get(&id).is_some() {
+            id = id +1;
+        }
+        // add the new entry to both maps
+        self.by_id.insert(id, val.clone());
+        self.by_value.insert(val.clone(), id);
+    
+        return id;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert_and_get() {
+        let mut s = StringStorage::new();
+        let first_id = s.add("abc".to_string());
+        assert!(s.str(first_id).is_some());
+    }
+}
---
+++ graphannis-base/src/lib.rs
@@ -0,0 +1,8 @@
+pub mod annis;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
