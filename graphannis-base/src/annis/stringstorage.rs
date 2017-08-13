e2da1f762ebdb23bdc52a52c0c7816873d6c8681
Thomas Krause <thomaskrause@posteo.de>
2017-08-13 10:14:35 +0200

    Use string slice as parameter to make it easier to use the add function in other code

--- graphannis-base/src/annis/stringstorage.rs
+++ graphannis-base/src/annis/stringstorage.rs
@@ -2,16 +2,15 @@ use std::collections::HashMap;
 use std::collections::BTreeMap;
 
 pub struct StringStorage {
-    by_id: HashMap<u32,String>,
-    by_value: BTreeMap<String, u32>
+    by_id: HashMap<u32, String>,
+    by_value: BTreeMap<String, u32>,
 }
 
 impl StringStorage {
-
     pub fn new() -> StringStorage {
         StringStorage {
             by_id: HashMap::new(),
-            by_value: BTreeMap::new()
+            by_value: BTreeMap::new(),
         }
     }
 
@@ -19,9 +18,9 @@ impl StringStorage {
         return self.by_id.get(&id);
     }
 
-    pub fn add(&mut self, val: String) -> u32 {
+    pub fn add(&mut self, val: &str) -> u32 {
         {
-            let existing = self.by_value.get(&val);
+            let existing = self.by_value.get(val);
             if existing.is_some() {
                 return *(existing.unwrap());
             }
@@ -29,13 +28,18 @@ impl StringStorage {
         // non-existing: add a new value
         let mut id = self.by_id.len() as u32 + 1; // since 0 is taken as ANY value begin with 1
         while self.by_id.get(&id).is_some() {
-            id = id +1;
+            id = id + 1;
         }
         // add the new entry to both maps
-        self.by_id.insert(id, val.clone());
-        self.by_value.insert(val.clone(), id);
-    
+        self.by_id.insert(id, String::from(val));
+        self.by_value.insert(String::from(val), id);
+
         return id;
+    }
+
+    pub fn clear(&mut self) {
+        self.by_id.clear();
+        self.by_value.clear();
     }
 }
 
@@ -46,10 +50,10 @@ mod tests {
     #[test]
     fn insert_and_get() {
         let mut s = StringStorage::new();
-        let id1 = s.add("abc".to_string());
-        let id2 = s.add("def".to_string());
-        let id3 = s.add("def".to_string());
-        
+        let id1 = s.add("abc");
+        let id2 = s.add("def");
+        let id3 = s.add("def");
+
         assert_eq!(id2, id3);
 
         let x = s.str(id1);
