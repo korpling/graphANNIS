0849f3b00732c1285d0d90489168ffebbea2fa5a
Thomas Krause <thomaskrause@posteo.de>
2017-08-27 19:59:11 +0200

    Implement simple get(...)

--- graphannis-rs/src/annis/annostorage.rs
+++ graphannis-rs/src/annis/annostorage.rs
@@ -2,26 +2,28 @@ use std::collections::{BTreeMap, BTreeSet};
 
 
 #[derive(Eq, PartialEq, PartialOrd, Ord, Clone)]
-pub struct AnnotationKey {
+pub struct AnnoKey {
     pub name: u32,
     pub ns: u32,
 }
 
 #[derive(Eq, PartialEq, PartialOrd, Ord, Clone)]
-pub struct ContainerAnnotationKey<T: Ord> {
+pub struct ContainerAnnoKey<T: Ord> {
     pub item: T,
-    pub key: AnnotationKey,
+    pub key: AnnoKey,
 }
 
 #[derive(Eq, PartialEq, PartialOrd, Ord, Clone)]
 pub struct Annotation {
-    pub key: AnnotationKey,
+    pub key: AnnoKey,
     pub val: u32,
 }
 
 pub struct AnnoStorage<T: Ord> {
-    by_container: BTreeMap<ContainerAnnotationKey<T>, u32>,
+    by_container: BTreeMap<ContainerAnnoKey<T>, u32>,
     by_anno: BTreeMap<Annotation, BTreeSet<T>>,
+    /// Maps a distinct annotation key to the number of keys available.
+    anno_keys: BTreeMap<AnnoKey, usize>,
 }
 
 impl<T: Ord + Clone> AnnoStorage<T> {
@@ -29,12 +31,13 @@ impl<T: Ord + Clone> AnnoStorage<T> {
         AnnoStorage {
             by_container: BTreeMap::new(),
             by_anno: BTreeMap::new(),
+            anno_keys: BTreeMap::new(),
         }
     }
 
-    pub fn add(&mut self, item: T, anno: &Annotation) {
+    pub fn insert(&mut self, item: T, anno: &Annotation) {
         self.by_container.insert(
-            ContainerAnnotationKey {
+            ContainerAnnoKey {
                 item: item.clone(),
                 key: anno.key.clone(),
             },
@@ -46,11 +49,24 @@ impl<T: Ord + Clone> AnnoStorage<T> {
         self.by_anno
             .entry(anno.clone())
             .or_insert(BTreeSet::new())
-            .insert(item.clone());
+            .insert(item);
+
+        let anno_key_entry = self.anno_keys.entry(anno.clone().key).or_insert(0);
+        *anno_key_entry = *anno_key_entry+1;
+    }
+
+    pub fn remove(&mut self, item: T) {
+        unimplemented!();
     }
 
     pub fn len(&self) -> usize {
         self.by_container.len()
+    }
+
+    pub fn get(&self, item: T, key: &AnnoKey) -> Option<&u32> {
+        let container_key = ContainerAnnoKey{item: item, key: key.clone()};
+
+        self.by_container.get(&container_key)
     }
 }
 
@@ -61,29 +77,22 @@ mod tests {
 
     #[test]
     fn insert_same_anno() {
+        let test_anno = Annotation {
+                key: AnnoKey { name: 1, ns: 1 },
+                val: 123,
+            };
         let mut a: AnnoStorage<u32> = AnnoStorage::new();
-        a.add(
-            1,
-            &Annotation {
-                key: AnnotationKey { name: 1, ns: 1 },
-                val: 123,
-            },
-        );
-        a.add(
-            2,
-            &Annotation {
-                key: AnnotationKey { name: 1, ns: 1 },
-                val: 123,
-            },
-        );
-        a.add(
-            3,
-            &Annotation {
-                key: AnnotationKey { name: 1, ns: 1 },
-                val: 123,
-            },
-        );
+        a.insert(1, &test_anno);
+        a.insert(1, &test_anno);
+        a.insert(2, &test_anno);
+        a.insert(3, &test_anno);
 
         assert_eq!(3, a.len());
+        assert_eq!(3, a.by_container.len());
+        assert_eq!(1, a.by_anno.len());
+        assert_eq!(1, a.anno_keys.len());
+
+        assert_eq!(123, a.get(3, &AnnoKey{name: 1, ns: 1}).unwrap().clone());
+
     }
 }
