cef0e6834c5f186c0602ec92d1a10b8606201155
Thomas Krause <thomaskrause@posteo.de>
2017-08-27 15:23:07 +0200

    Also create dynamic library and only compile rust as release

--- cmake/graphannis-rs.cmake
+++ cmake/graphannis-rs.cmake
@@ -2,10 +2,10 @@ ExternalProject_Add(
     graphannis-rs
     DOWNLOAD_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND cargo build COMMAND cargo build --release --all
+    BUILD_COMMAND cargo build --release --all
     BINARY_DIR "${CMAKE_SOURCE_DIR}/graphannis-rs"
     INSTALL_COMMAND ""
-    LOG_BUILD ON)
+    LOG_BUILD OFF)
 
   set(GRAPHANNISRS_LIBRARIES "${CMAKE_SOURCE_DIR}/graphannis-rs/target/release/${CMAKE_STATIC_LIBRARY_PREFIX}graphannis${CMAKE_STATIC_LIBRARY_SUFFIX}"
     "util" "dl" "rt" "pthread" "c" "m" "rt" "pthread" "util")
