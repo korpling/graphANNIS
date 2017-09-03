0b11b371a34596b469bb77859042e634410e1891
Thomas Krause <thomaskrause@posteo.de>
2017-09-03 22:35:37 +0200

    Implement a function that returns an iterator for exact annotation key search

--- graphannis-rs/src/annis/annostorage/mod.rs
+++ graphannis-rs/src/annis/annostorage/mod.rs
@@ -1,6 +1,6 @@
-use annis::StringID;
+use super::*;
 use std::collections::{BTreeMap, BTreeSet};
-
+use std;
 
 #[derive(Eq, PartialEq, PartialOrd, Ord, Clone, Debug)]
 pub struct AnnoKey {
@@ -20,12 +20,19 @@ pub struct Annotation {
     pub val: StringID,
 }
 
+#[derive(Debug)]
+pub struct Match {
+    node: NodeID,
+    anno: Annotation,
+}
+
 pub struct AnnoStorage<T: Ord> {
     by_container: BTreeMap<ContainerAnnoKey<T>, StringID>,
     by_anno: BTreeMap<Annotation, BTreeSet<T>>,
     /// Maps a distinct annotation key to the number of keys available.
     anno_keys: BTreeMap<AnnoKey, usize>,
 }
+
 
 impl<T: Ord + Clone> AnnoStorage<T> {
     pub fn new() -> AnnoStorage<T> {
@@ -63,7 +70,10 @@ impl<T: Ord + Clone> AnnoStorage<T> {
         });
         if old_value.is_some() {
             // of value was found, also remove the item from the other containers
-            self.by_anno.remove(&Annotation{key: key.clone(), val: old_value.unwrap()});
+            self.by_anno.remove(&Annotation {
+                key: key.clone(),
+                val: old_value.unwrap(),
+            });
             // decrease the annotation count for this key
             let num_of_keys = self.anno_keys.get_mut(key);
             if num_of_keys.is_some() {
@@ -74,7 +84,6 @@ impl<T: Ord + Clone> AnnoStorage<T> {
             return old_value;
         }
         return None;
-
     }
 
     pub fn len(&self) -> usize {
@@ -116,6 +125,36 @@ impl<T: Ord + Clone> AnnoStorage<T> {
         }
 
         return result;
+    }
+}
+
+impl<'a> AnnoStorage<NodeID> {
+    pub fn exact_annokey_search(&'a self, namespace : Option<StringID>, name : StringID) -> Box<Iterator<Item = Match >+ 'a> {
+        let ns_pair = match namespace {
+            Some(v) => (v,v),
+            None => (StringID::min_value(), StringID::max_value()),
+        };
+
+        let anno_min = Annotation {
+            key: AnnoKey{name, ns: ns_pair.0},
+            val: 0,
+        };
+        let anno_max = Annotation {
+            key: AnnoKey{name, ns: ns_pair.1},
+            val: NodeID::max_value(),
+        };
+
+        Box::new(
+            self.by_anno
+                .range(anno_min..anno_max)
+                .flat_map(|nodes| nodes.1.iter().zip(std::iter::repeat(nodes.0)))
+                .map(|m| {
+                    Match {
+                        node: m.0.clone(),
+                        anno: m.1.clone(),
+                    }
+                }),
+        )
     }
 }
 
