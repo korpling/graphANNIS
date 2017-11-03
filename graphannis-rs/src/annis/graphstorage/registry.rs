17acd115c312936fb8afbb922369f763525045f2
Thomas Krause <thomaskrause@posteo.de>
2017-11-03 19:52:12 +0100

    Implement deserialize by name

--- graphannis-rs/src/annis/graphstorage/registry.rs
+++ graphannis-rs/src/annis/graphstorage/registry.rs
@@ -3,6 +3,7 @@ use std::string::String;
 use super::{WriteableGraphStorage, ReadableGraphStorage};
 use super::adjacencylist::AdjacencyListStorage;
 use std;
+use bincode;
 
 #[derive(Debug)]
 pub enum RegistryError {
@@ -14,12 +15,13 @@ type Result<T> = std::result::Result<T, RegistryError>;
 
 enum Type {
     Readable{
-        copy_constructor : fn(&ReadableGraphStorage) -> Box<ReadableGraphStorage>
+        copy_constructor : fn(&ReadableGraphStorage) -> Box<ReadableGraphStorage>,
+        deserializer : fn(&mut std::io::Read) -> Box<ReadableGraphStorage>
     },
     Writable{
         copy_constructor : fn(&ReadableGraphStorage) -> Box<WriteableGraphStorage>, 
         empty_constructor : fn() -> Box<WriteableGraphStorage>,
-        deserializer : fn(&std::io::Read) -> Box<WriteableGraphStorage>,
+        deserializer : fn(&mut std::io::Read) -> Box<ReadableGraphStorage>,
     },
 }
 
@@ -36,7 +38,12 @@ impl GraphStorageRegistry {
         types.insert(String::from("AdjacencyListStorage"), Type::Writable{
             copy_constructor : |orig : &ReadableGraphStorage| Box::new(AdjacencyListStorage::new()),
             empty_constructor : || Box::new(AdjacencyListStorage::new()),
-            deserializer : |input : &std::io::Read| Box::new(AdjacencyListStorage::new()),
+            deserializer : |input : &mut std::io::Read| {
+                let loaded = 
+                    bincode::deserialize_from(input, bincode::Infinite)
+                    .unwrap_or(Box::new(AdjacencyListStorage::new()));
+                return loaded;
+            },
         });
         // TODO: add other graph storages
 
@@ -53,7 +60,18 @@ impl GraphStorageRegistry {
         Err(RegistryError::Empty)
     }
 
-    pub fn load_by_name(&self) {
+    pub fn load_by_name(&self, impl_name : &str, input : &mut std::io::Read) -> Result<Box<ReadableGraphStorage>> {
+        let t = try!(self.types.get(impl_name).ok_or(RegistryError::ImplementationNameNotFound));
 
+        let gs = match *t {
+            Type::Readable{copy_constructor, deserializer} => {
+                deserializer(input)
+            },
+            Type::Writable{copy_constructor, empty_constructor, deserializer} => {
+                deserializer(input)
+            }
+        };
+
+        Ok(gs)
     }
 }
