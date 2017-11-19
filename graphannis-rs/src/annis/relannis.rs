1fd8148b3abce98008f12f1bc58c08b92ffc53ae
Thomas Krause <thomaskrause@posteo.de>
2017-11-19 14:51:47 +0100

    Implement load_node_anno_tab

--- graphannis-rs/src/annis/relannis.rs
+++ graphannis-rs/src/annis/relannis.rs
@@ -1,5 +1,5 @@
 use graphdb::GraphDB;
-use annis::{AnnoKey, Annotation, NodeID, Component, ComponentType, Edge};
+use annis::{AnnoKey, Annotation, NodeID, StringID, Component, ComponentType, Edge};
 use annis::graphstorage::WriteableGraphStorage;
 use annis;
 use std::path::{Path, PathBuf};
@@ -108,71 +108,100 @@ fn parse_corpus_tab(
     toplevel_corpus_name.ok_or(Error::ToplevelCorpusNameNotFound)
 }
 
-fn calculate_automatic_token_info( db: &mut GraphDB,
-    token_by_index : &BTreeMap<TextProperty, NodeID>,
+fn calculate_automatic_token_info(
+    db: &mut GraphDB,
+    token_by_index: &BTreeMap<TextProperty, NodeID>,
     node_to_left: &BTreeMap<NodeID, u32>,
     node_to_right: &BTreeMap<NodeID, u32>,
     left_to_node: &MultiMap<TextProperty, NodeID>,
-    right_to_node: &MultiMap<TextProperty, NodeID>
+    right_to_node: &MultiMap<TextProperty, NodeID>,
 ) -> Result<()> {
+
+    // TODO: cleanup, better variable naming
+    // iterate over all token by their order, find the nodes with the same
+    // text coverage (either left or right) and add explicit ORDERING, LEFT_TOKEN and RIGHT_TOKEN edges
 
     info!("calculating the automatically generated ORDERING, LEFT_TOKEN and RIGHT_TOKEN edges");
 
-    let mut last_textprop : Option<TextProperty> = None;
-    let mut last_token : Option<NodeID> = None;
+    let mut last_textprop: Option<TextProperty> = None;
+    let mut last_token: Option<NodeID> = None;
 
-    let component_left = Component{ctype: ComponentType::LeftToken, 
-            layer: String::from("annis"), name: String::from("")};
-    let component_right = Component{ctype: ComponentType::RightToken, 
-        layer: String::from("annis"), name: String::from("")};
+    let component_left = Component {
+        ctype: ComponentType::LeftToken,
+        layer: String::from("annis"),
+        name: String::from(""),
+    };
+    let component_right = Component {
+        ctype: ComponentType::RightToken,
+        layer: String::from("annis"),
+        name: String::from(""),
+    };
 
-    for (current_textprop, current_token) in  token_by_index {
+    for (current_textprop, current_token) in token_by_index {
 
         if current_textprop.segmentation == "" {
             // find all nodes that start together with the current token
-            let current_token_left = TextProperty{
-                segmentation: String::from(""), 
-                text_id : current_textprop.text_id, 
-                corpus_id : current_textprop.corpus_id,
-                val : try!(node_to_left.get(&current_token).ok_or(Error::Other)).clone(),
+            let current_token_left = TextProperty {
+                segmentation: String::from(""),
+                text_id: current_textprop.text_id,
+                corpus_id: current_textprop.corpus_id,
+                val: try!(node_to_left.get(&current_token).ok_or(Error::Other)).clone(),
             };
             let left_aligned = left_to_node.get_vec(&current_token_left);
             if left_aligned.is_some() {
                 let gs_left = db.get_or_create_writable(component_left.clone())?;
 
                 for n in left_aligned.unwrap() {
-                    gs_left.add_edge(Edge{source: n.clone(), target: current_token.clone()});
-                    gs_left.add_edge(Edge{source: current_token.clone(), target: n.clone()});
+                    gs_left.add_edge(Edge {
+                        source: n.clone(),
+                        target: current_token.clone(),
+                    });
+                    gs_left.add_edge(Edge {
+                        source: current_token.clone(),
+                        target: n.clone(),
+                    });
                 }
             }
             // find all nodes that end together with the current token
-            let current_token_right = TextProperty{
-                segmentation: String::from(""), 
-                text_id : current_textprop.text_id, 
-                corpus_id : current_textprop.corpus_id,
-                val : try!(node_to_right.get(current_token).ok_or(Error::Other)).clone(),
+            let current_token_right = TextProperty {
+                segmentation: String::from(""),
+                text_id: current_textprop.text_id,
+                corpus_id: current_textprop.corpus_id,
+                val: try!(node_to_right.get(current_token).ok_or(Error::Other)).clone(),
             };
             let right_aligned = right_to_node.get_vec(&current_token_right);
             if right_aligned.is_some() {
                 let gs_right = db.get_or_create_writable(component_right.clone())?;
                 for n in right_aligned.unwrap() {
-                    gs_right.add_edge(Edge{source: n.clone(), target: current_token.clone()});
-                    gs_right.add_edge(Edge{source: current_token.clone(), target: n.clone()});
+                    gs_right.add_edge(Edge {
+                        source: n.clone(),
+                        target: current_token.clone(),
+                    });
+                    gs_right.add_edge(Edge {
+                        source: current_token.clone(),
+                        target: n.clone(),
+                    });
                 }
             }
         } // end if current segmentation is default
 
-        let component_order = Component{ctype: ComponentType::Ordering, 
-            layer: String::from("annis"), name: current_textprop.segmentation.clone()};
+        let component_order = Component {
+            ctype: ComponentType::Ordering,
+            layer: String::from("annis"),
+            name: current_textprop.segmentation.clone(),
+        };
 
         let gs_order = db.get_or_create_writable(component_order.clone())?;
-        
+
         // if the last token/text value is valid and we are still in the same text
-        if  last_token.is_some()
-            && last_textprop.is_some()
-            && last_textprop.unwrap() == current_textprop.clone() {
+        if last_token.is_some() && last_textprop.is_some() &&
+            last_textprop.unwrap() == current_textprop.clone()
+        {
             // we are still in the same text, add ordering between token
-            gs_order.add_edge(Edge{source: last_token.unwrap(), target: current_token.clone()});
+            gs_order.add_edge(Edge {
+                source: last_token.unwrap(),
+                target: current_token.clone(),
+            });
         } // end if same text
 
         // update the iterator and other variables
@@ -185,83 +214,97 @@ fn calculate_automatic_token_info( db: &mut GraphDB,
     Ok(())
 }
 
-fn calculate_automatic_coverage_edges(db: &mut GraphDB,
-    token_by_index : &BTreeMap<TextProperty, NodeID>,
+fn calculate_automatic_coverage_edges(
+    db: &mut GraphDB,
+    token_by_index: &BTreeMap<TextProperty, NodeID>,
     token_to_index: &BTreeMap<NodeID, TextProperty>,
     node_to_right: &BTreeMap<NodeID, u32>,
     left_to_node: &MultiMap<TextProperty, NodeID>,
-    right_to_node: &MultiMap<TextProperty, NodeID>,
     token_by_left_textpos: &BTreeMap<TextProperty, NodeID>,
     token_by_right_textpos: &BTreeMap<TextProperty, NodeID>,
-    ) 
- -> Result<()> {
+) -> Result<()> {
 
-     // add explicit coverage edges for each node in the special annis namespace coverage component
-    let component_coverage = Component{ctype: ComponentType::Coverage, 
-                layer: String::from("annis"), name: String::from("")};
-    let component_inv_cov = Component{ctype: ComponentType::InverseCoverage, 
-                layer: String::from("annis"), name: String::from("")};
+    // add explicit coverage edges for each node in the special annis namespace coverage component
+    let component_coverage = Component {
+        ctype: ComponentType::Coverage,
+        layer: String::from("annis"),
+        name: String::from(""),
+    };
+    let component_inv_cov = Component {
+        ctype: ComponentType::InverseCoverage,
+        layer: String::from("annis"),
+        name: String::from(""),
+    };
     {
         info!("calculating the automatically generated COVERAGE edges");
         for (textprop, n_vec) in left_to_node {
             for n in n_vec {
                 if !token_to_index.contains_key(&n) {
-                    
-                    let left_pos = TextProperty{
-                        segmentation : String::from(""),
-                        corpus_id : textprop.corpus_id,
-                        text_id : textprop.text_id,
-                        val : textprop.val,
+
+                    let left_pos = TextProperty {
+                        segmentation: String::from(""),
+                        corpus_id: textprop.corpus_id,
+                        text_id: textprop.text_id,
+                        val: textprop.val,
                     };
                     let right_pos = node_to_right.get(&n).ok_or(Error::Other)?;
-                    let right_pos = TextProperty{
-                        segmentation : String::from(""),
-                        corpus_id : textprop.corpus_id,
-                        text_id : textprop.text_id,
-                        val : right_pos.clone(),
+                    let right_pos = TextProperty {
+                        segmentation: String::from(""),
+                        corpus_id: textprop.corpus_id,
+                        text_id: textprop.text_id,
+                        val: right_pos.clone(),
                     };
 
                     // find left/right aligned basic token
-                    let left_aligned_tok = token_by_left_textpos.get(&left_pos).ok_or(Error::Other)?;
-                    let right_aligned_tok = token_by_right_textpos.get(&right_pos).ok_or(Error::Other)?;
+                    let left_aligned_tok =
+                        token_by_left_textpos.get(&left_pos).ok_or(Error::Other)?;
+                    let right_aligned_tok =
+                        token_by_right_textpos.get(&right_pos).ok_or(Error::Other)?;
 
                     let left_tok_pos = token_to_index.get(&left_aligned_tok).ok_or(Error::Other)?;
                     let right_tok_pos = token_to_index.get(&right_aligned_tok).ok_or(Error::Other)?;
-                    for i in left_tok_pos.val..(right_tok_pos.val+1) {
-                        let tok_idx = TextProperty{
-                            segmentation : String::from(""),
-                            corpus_id : textprop.corpus_id,
-                            text_id : textprop.text_id,
-                            val : i,
+                    for i in left_tok_pos.val..(right_tok_pos.val + 1) {
+                        let tok_idx = TextProperty {
+                            segmentation: String::from(""),
+                            corpus_id: textprop.corpus_id,
+                            text_id: textprop.text_id,
+                            val: i,
                         };
                         let tok_id = token_by_index.get(&tok_idx).ok_or(Error::Other)?;
                         if n.clone() != tok_id.clone() {
                             {
                                 let gs = db.get_or_create_writable(component_coverage.clone())?;
-                                gs.add_edge(Edge{source: n.clone(), target: tok_id.clone()});
+                                gs.add_edge(Edge {
+                                    source: n.clone(),
+                                    target: tok_id.clone(),
+                                });
                             }
                             {
                                 let gs = db.get_or_create_writable(component_inv_cov.clone())?;
-                                gs.add_edge(Edge{source: tok_id.clone(), target: n.clone()});
+                                gs.add_edge(Edge {
+                                    source: tok_id.clone(),
+                                    target: n.clone(),
+                                });
                             }
                         }
-                    }      
+                    }
                 } // end if not a token
             }
-        }            
+        }
     }
 
-     Ok(())
- }
+    Ok(())
+}
 
-fn load_node_tab(path: &PathBuf,
+fn load_node_tab(
+    path: &PathBuf,
     db: &mut GraphDB,
     nodes_by_corpus_id: &mut MultiMap<u32, NodeID>,
     corpus_id_to_name: &mut BTreeMap<u32, String>,
     missing_seg_span: &mut BTreeMap<NodeID, String>,
     toplevel_corpus_name: &str,
-    is_annis_33: bool) 
--> Result<()> {
+    is_annis_33: bool,
+) -> Result<()> {
 
     let mut node_tab_path = PathBuf::from(path);
     node_tab_path.push(if is_annis_33 {
@@ -307,9 +350,9 @@ fn load_node_tab(path: &PathBuf,
             let layer: &str = line.get(3).ok_or(Error::MissingColumn)?;
             let node_name = line.get(4).ok_or(Error::MissingColumn)?;
 
-            let doc_name = corpus_id_to_name
-                .get(&corpus_id)
-                .ok_or(Error::DocumentMissing)?;
+            let doc_name = corpus_id_to_name.get(&corpus_id).ok_or(
+                Error::DocumentMissing,
+            )?;
             nodes_by_corpus_id.insert(corpus_id, node_nr);
 
             let node_qname = format!("{}/{}#{}", toplevel_corpus_name, doc_name, node_name);
@@ -362,15 +405,15 @@ fn load_node_tab(path: &PathBuf,
                     line.get(9).ok_or(Error::MissingColumn)?
                 };
 
-                let tok_anno = Annotation{
-                    key : db.get_token_key(),
-                    val : db.strings.add(span),
+                let tok_anno = Annotation {
+                    key: db.get_token_key(),
+                    val: db.strings.add(span),
                 };
                 db.node_annos.insert(node_nr, tok_anno);
 
                 let index = TextProperty {
-                    segmentation : String::from(""),
-                    val : token_index_raw.parse::<u32>()?,
+                    segmentation: String::from(""),
+                    val: token_index_raw.parse::<u32>()?,
                     text_id,
                     corpus_id,
                 };
@@ -396,8 +439,8 @@ fn load_node_tab(path: &PathBuf,
                     if is_annis_33 {
                         // directly add the span information
                         let tok_anno = Annotation {
-                            key : db.get_token_key(),
-                            val : db.strings.add(line.get(12).ok_or(Error::MissingColumn)?),
+                            key: db.get_token_key(),
+                            val: db.strings.add(line.get(12).ok_or(Error::MissingColumn)?),
                         };
                         db.node_annos.insert(node_nr, tok_anno);
                     } else {
@@ -406,8 +449,8 @@ fn load_node_tab(path: &PathBuf,
                     }
                     // also add the specific segmentation index
                     let index = TextProperty {
-                        segmentation : String::from(segmentation_name),
-                        val : seg_index,
+                        segmentation: String::from(segmentation_name),
+                        val: seg_index,
                         corpus_id,
                         text_id,
                     };
@@ -419,20 +462,87 @@ fn load_node_tab(path: &PathBuf,
         }
     } // end "scan all lines" visibility block
 
-    // TODO: cleanup, better variable naming and put this into it's own function
-    // iterate over all token by their order, find the nodes with the same
-    // text coverage (either left or right) and add explicit ORDERING, LEFT_TOKEN and RIGHT_TOKEN edges
     if !token_by_index.is_empty() {
-        calculate_automatic_token_info(db, &token_by_index, &node_to_left, &node_to_right, &left_to_node, &right_to_node)?;
+        calculate_automatic_token_info(
+            db,
+            &token_by_index,
+            &node_to_left,
+            &node_to_right,
+            &left_to_node,
+            &right_to_node,
+        )?;
     } // end if token_by_index not empty
 
-    calculate_automatic_coverage_edges(db, &token_by_index, &token_to_index, 
-        &node_to_right, &left_to_node, &right_to_node, &token_by_left_textpos, &token_by_right_textpos)?;
+    calculate_automatic_coverage_edges(
+        db,
+        &token_by_index,
+        &token_to_index,
+        &node_to_right,
+        &left_to_node,
+        &token_by_left_textpos,
+        &token_by_right_textpos,
+    )?;
     Ok(())
 }
 
-fn load_node_anno_tab() -> Result<()> {
-    unimplemented!();
+fn load_node_anno_tab(path: &PathBuf, db: &mut GraphDB, 
+    missing_seg_span : &BTreeMap<NodeID, String>, 
+    is_annis_33: bool) -> Result<()> {
+
+    let mut node_anno_tab_path = PathBuf::from(path);
+    node_anno_tab_path.push(if is_annis_33 {
+        "node_annotation.annis"
+    } else {
+        "node_annotation.tab"
+    });
+
+    info!(
+        "loading {}",
+        node_anno_tab_path.to_str().unwrap_or_default()
+    );
+
+    let mut node_anno_tab_csv = postgresql_import_reader(node_anno_tab_path.as_path())?;
+
+    for result in node_anno_tab_csv.records() {
+        let line = result?;
+    
+        let col_id = line.get(0).ok_or(Error::MissingColumn)?;
+        let node_id : NodeID = col_id.parse()?;
+        let col_ns = line.get(1).ok_or(Error::MissingColumn)?;
+        let col_name = line.get(2).ok_or(Error::MissingColumn)?;
+        let col_val = line.get(3).ok_or(Error::MissingColumn)?;
+        // we have to make some sanity checks
+        if col_ns != "annis" ||
+            col_name != "tok"
+        {
+            let anno_val = if col_val == "NULL" {
+                // use an "invalid" string so it can't be found by its value, but only by its annotation name
+                <StringID>::max_value()
+            } else {
+                db.strings.add(col_val)
+            };
+
+            db.node_annos.insert(node_id.clone(), Annotation{
+                key : AnnoKey {ns : db.strings.add(col_ns), name : db.strings.add(col_name)},
+                val : anno_val,
+            });
+
+            // add all missing span values from the annotation, but don't add NULL values
+            if let Some(seg) = missing_seg_span.get(&node_id) {
+                if seg == line.get(2).ok_or(Error::MissingColumn)?
+                    && line.get(3).ok_or(Error::MissingColumn)? != "NULL" {
+               
+                    let tok_key = db.get_token_key();
+                    db.node_annos.insert(node_id.clone(), Annotation{
+                        key : tok_key,
+                        val : anno_val,
+                    });
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
 fn load_nodes(
@@ -445,8 +555,17 @@ fn load_nodes(
 ) -> Result<()> {
 
     let mut missing_seg_span: BTreeMap<NodeID, String> = BTreeMap::new();
-    
-    load_node_tab(path, db, nodes_by_corpus_id, corpus_id_to_name, &mut missing_seg_span, toplevel_corpus_name, is_annis_33)?;
+
+    load_node_tab(
+        path,
+        db,
+        nodes_by_corpus_id,
+        corpus_id_to_name,
+        &mut missing_seg_span,
+        toplevel_corpus_name,
+        is_annis_33,
+    )?;
+    load_node_anno_tab(path, db, &missing_seg_span, is_annis_33)?;
 
 
     return Ok(());
