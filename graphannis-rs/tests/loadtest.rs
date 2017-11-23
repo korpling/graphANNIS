6bffa6fe646d4779a34b7f03f7d5e38bd10432c6
Thomas Krause <thomaskrause@posteo.de>
2017-11-23 17:34:13 +0100

    Extend test to retrieve some annotations

--- graphannis-rs/tests/loadtest.rs
+++ graphannis-rs/tests/loadtest.rs
@@ -4,6 +4,7 @@ use std::env;
 use std::path::PathBuf;
 use graphannis::graphdb::GraphDB;
 use graphannis::relannis;
+use graphannis::{Annotation};
 
 fn load_corpus(name : &str) -> Option<GraphDB> {
     let mut data_dir = PathBuf::from(if let Ok(path) = env::var("ANNIS4_TEST_DATA") {
@@ -25,6 +26,16 @@ fn load_corpus(name : &str) -> Option<GraphDB> {
 #[test]
 fn node_annos() {
     if let Some(db) = load_corpus("pcc2") {
+        let annos : Vec<Annotation> = db.node_annos.get_all(&0);
 
+        assert_eq!(7, annos.len());
+        
+        assert_eq!("annis", db.strings.str(annos[0].key.ns).unwrap());
+        assert_eq!("node_name", db.strings.str(annos[0].key.name).unwrap());
+        assert_eq!("pcc2/4282#tok_13", db.strings.str(annos[0].val).unwrap());
+
+        assert_eq!("annis", db.strings.str(annos[1].key.ns).unwrap());
+        assert_eq!("tok", db.strings.str(annos[1].key.name).unwrap());
+        assert_eq!("so", db.strings.str(annos[1].val).unwrap());
     }
 }
