e10a8c72c688529c98b39750f839f543e39258fe
Thomas Krause <thomaskrause@posteo.de>
2017-12-10 19:26:11 +0100

    Index join now returns Vec<Match> instead of a tuple

--- graphannis-rs/src/join/indexjoin.rs
+++ graphannis-rs/src/join/indexjoin.rs
@@ -4,13 +4,15 @@ use std::rc::Rc;
 use std;
 
 pub fn new<'a>(
-    lhs: Box<Iterator<Item = Match>>,
+    lhs: Box<Iterator<Item = Vec<Match>>>,
+    lhs_idx : usize,
     op: Rc<Operator>,
     anno_cond: Box<Fn(Annotation) -> bool + 'a>,
-) -> Box<Iterator<Item = (Match, Match)> + 'a> {
+) -> Box<Iterator<Item = Vec<Match>> + 'a> {
     let it = lhs.flat_map(move |m_lhs| {
-        std::iter::repeat(m_lhs.clone()).zip(op.retrieve_matches(&m_lhs))
-    }).filter(move |m| anno_cond(m.1.anno.clone()));
+        std::iter::repeat(m_lhs.clone()).zip(op.retrieve_matches(&m_lhs[lhs_idx]))
+    }).filter(move |m| anno_cond(m.1.anno.clone()))
+    .map(|match_pair| {let mut result = match_pair.0.clone(); result.push(match_pair.1); result});
 
     return Box::new(it);
 }
