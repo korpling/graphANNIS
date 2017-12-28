8ba3af4692fd414c8e37ceb79ed9032ef09f93c0
Thomas Krause <thomaskrause@posteo.de>
2017-12-28 14:19:45 +0100

    Add function to get corpus from cache

--- graphannis-rs/src/api/corpusstorage.rs
+++ graphannis-rs/src/api/corpusstorage.rs
@@ -1,17 +1,53 @@
-//! An API for managing a so-called corpus-storage, which is a folder on the file system that contains multiple corpora.
-//! This API is transactional and allows multiple reads for the same corpus at the same time.
+//! An API for managing corpora stored in a common location on the file system.
+//! It is transactional and thread-safe.
 
-use std::path::PathBuf;
+use std::sync::{Arc,RwLock};
+use std::path::{PathBuf, Path};
+use std::collections::BTreeMap;
+use graphdb::GraphDB;
+//use {Annotation, Match, NodeID, StringID, AnnoKey};
+
+#[derive(Clone)]
+enum LoadStatus {
+    NotLoaded(PathBuf),
+    NodesLoaded(Arc<GraphDB>),
+    FullyLoaded(Arc<GraphDB>),
+}
 
 pub struct CorpusStorage {
     db_dir : PathBuf,
+    max_allowed_cache_size : Option<usize>,
+
+    corpus_cache: RwLock<BTreeMap<String, LoadStatus>>,
 }
 
 impl CorpusStorage {
-    pub fn new(db_dir : PathBuf) -> CorpusStorage {
+    pub fn new(db_dir : PathBuf, max_allowed_cache_size : Option<usize>) -> CorpusStorage {
 
         CorpusStorage {
             db_dir,
+            max_allowed_cache_size,
+            corpus_cache: RwLock::new(BTreeMap::new()),
         }
+    }
+
+    fn get_corpus_from_cache(&mut self, corpus_name : &str) -> LoadStatus {
+        let mut cache_lock =  self.corpus_cache.write().unwrap();
+        
+        let cache : &mut BTreeMap<String, LoadStatus> = &mut *cache_lock;
+        
+        let entry = cache.entry(String::from(corpus_name)).or_insert_with(|| {
+            // Create a new LoadStatus and put it into the cache. This will not load
+            // the database itself, this can be done with the resulting object from the caller.
+            let db_path : PathBuf = [self.db_dir.to_string_lossy().as_ref(), corpus_name].iter().collect();
+            LoadStatus::NotLoaded(db_path)
+        });
+        return entry.clone();
+    }
+
+    /// Import a corpus from an external location into this corpus storage
+    pub fn import(&mut self, path_to_corpus : &Path, new_corpus_name : &str) {
+        let corpus = self.get_corpus_from_cache(new_corpus_name);
+        unimplemented!()
     }
 }
