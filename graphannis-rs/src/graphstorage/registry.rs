658e0818893fc841c0b27fa73a4235badb3d5f0d
Thomas Krause <thomaskrause@posteo.de>
2017-12-29 01:35:28 +0100

    Use enum to store names of all known implementations

--- graphannis-rs/src/graphstorage/registry.rs
+++ graphannis-rs/src/graphstorage/registry.rs
@@ -4,6 +4,8 @@ use std;
 use std::rc::Rc;
 use bincode;
 use std::any::Any;
+use std::str::FromStr;
+use strum;
 
 #[derive(Debug)]
 pub enum RegistryError {
@@ -14,9 +16,20 @@ pub enum RegistryError {
     Other,
 }
 
+#[derive(ToString, EnumString)]
+enum ImplTypes {
+    adjacency_v1,
+}
+
 impl From<Box<bincode::ErrorKind>> for RegistryError {
     fn from(e: Box<bincode::ErrorKind>) -> RegistryError {
         RegistryError::Serialization(e)
+    }
+}
+
+impl From<strum::ParseError> for RegistryError {
+    fn from(e: strum::ParseError) -> RegistryError {
+        RegistryError::ImplementationNameNotFound
     }
 }
 
@@ -29,19 +42,20 @@ pub fn create_writeable() -> AdjacencyListStorage {
 
 pub fn load_by_name(impl_name : &str, input : &mut std::io::Read) -> Result<Rc<GraphStorage>> {
 
-    match impl_name {
-        "AdjacencyListStorage" => {
+    let impl_type = ImplTypes::from_str(impl_name)?;
+
+    match impl_type {
+        ImplTypes::adjacency_v1 => {
             let gs : AdjacencyListStorage =  bincode::deserialize_from(input, bincode::Infinite)?;
             Ok(Rc::new(gs))
-        },
-        _ => Err(RegistryError::ImplementationNameNotFound)
+        }
     }
 }
 
-pub fn serialize(data : &Any, writer : &mut std::io::Write) -> Result<&'static str> {
-    if let Some(adja) = data.downcast_ref::<AdjacencyListStorage>() {
-        bincode::serialize_into(writer, adja, bincode::Infinite)?;
-        return Ok("AdjacencyListStorage");
+pub fn serialize(data : &Any, writer : &mut std::io::Write) -> Result<String> {
+    if let Some(adja) = data.downcast_ref::<Rc<AdjacencyListStorage>>() {
+        bincode::serialize_into(writer, adja.as_ref(), bincode::Infinite)?;
+        return Ok(ImplTypes::adjacency_v1.to_string());
     }
     return Err(RegistryError::TypeNotFound);
 }
