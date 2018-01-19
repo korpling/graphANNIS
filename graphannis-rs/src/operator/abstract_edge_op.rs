41df2090b431ccec141efdb5ace2fad126080a65
Thomas Krause <thomaskrause@posteo.de>
2018-01-19 17:47:06 +0100

    Implement filter function for AbstractEdgeOp

--- graphannis-rs/src/operator/abstract_edge_op.rs
+++ graphannis-rs/src/operator/abstract_edge_op.rs
@@ -1,7 +1,8 @@
-use {Annotation, Component, Match};
+use {Annotation, Component, Match, NodeID, Edge};
 use graphstorage::GraphStorage;
 use graphdb::GraphDB;
 use operator::{OperatorSpec, Operator};
+use util;
 use std::rc::Rc;
 
 #[derive(Clone)]
@@ -47,6 +48,28 @@ impl OperatorSpec for AbstractEdgeOpSpec {
     }
 }
 
+impl AbstractEdgeOp {
+    fn check_edge_annotation(&self, gs : &GraphStorage, source : &NodeID, target : &NodeID) -> bool {
+        if self.spec.edge_anno.is_none() {
+            return true;
+        }
+
+        let anno_template : &Annotation = self.spec.edge_anno.as_ref().unwrap();
+        if anno_template.val == 0 || anno_template.val == <NodeID>::max_value() {
+            // must be a valid value
+            return false;
+        } else {
+            // check if the edge has the correct annotation first
+            for a in gs.get_edge_annos(&Edge {source: source.clone(), target: target.clone()}) {
+                if util::check_annotation_equal(&anno_template, &a) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
 impl Operator for AbstractEdgeOp {
 
     fn retrieve_matches<'b>(&'b self, lhs: &Match) -> Box<Iterator<Item = Match> + 'b> {
@@ -55,6 +78,13 @@ impl Operator for AbstractEdgeOp {
     }
 
     fn filter_match(&self, lhs: &Match, rhs: &Match) -> bool {
-        unimplemented!()
+        for e in self.gs.iter() {
+            if e.is_connected(&lhs.node, &rhs.node, self.spec.min_dist, self.spec.max_dist) {
+                if self.check_edge_annotation(e.as_ref(), &lhs.node, &rhs.node) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- graphannis-rs/src/util/mod.rs
+++ graphannis-rs/src/util/mod.rs
@@ -4,12 +4,33 @@ pub mod memory_estimation;
 #[macro_use]
 pub mod c_api;
 
-pub fn regex_full_match(pattern : &str) -> String {
-    
+use Annotation;
+
+pub fn regex_full_match(pattern: &str) -> String {
     let mut full_match_pattern = String::new();
     full_match_pattern.push_str(r"\A");
     full_match_pattern.push_str(pattern);
-    full_match_pattern.push_str(r"\z");   
+    full_match_pattern.push_str(r"\z");
 
-    full_match_pattern 
+    full_match_pattern
+}
+
+pub fn check_annotation_equal(a: &Annotation, b: &Annotation) -> bool {
+    // compare by name (non lexical but just by the ID)
+    if a.key.name != 0 && b.key.name != 0 && a.key.name != b.key.name {
+        return false;
+    }
+
+    // if equal, compare by namespace (non lexical but just by the ID)
+    if a.key.ns != 0 && b.key.ns != 0 && a.key.ns != b.key.ns {
+        return false;
+    }
+
+    // if still equal compare by value (non lexical but just by the ID)
+    if a.val != 0 && b.val != 0 && a.val != b.val {
+        return false;
+    }
+
+    // they are equal
+    return true;
 }
