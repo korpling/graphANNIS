0a16d0e2bc1fb392be7b84d8ddb9ae4c0624cb9e
Thomas Krause <thomaskrause@posteo.de>
2018-02-21 17:53:30 +0100

    Handle filter case: can't use right exec node to get idx because it is already removed

--- graphannis/src/query/conjunction.rs
+++ graphannis/src/query/conjunction.rs
@@ -102,7 +102,6 @@ impl<'a> Conjunction<'a> {
         mut self,
         db: &'a GraphDB,
     ) -> Result<Box<ExecutionNode<Item = Vec<Match>> + 'a>, Error> {
-        // TODO: handle cost estimations
         // TODO: parallization mapping
 
         let mut node2component: BTreeMap<usize, usize> = BTreeMap::new();
@@ -116,12 +115,20 @@ impl<'a> Conjunction<'a> {
         {
             let mut node_nr: usize = 0;
             for n_spec in self.nodes.drain(..) {
-                let mut n =
-                    NodeSearch::from_spec(n_spec.clone(), node_nr, db).ok_or(Error::ImpossibleSearch(format!("could not create node search for node {} ({})", node_nr, n_spec)))?;
+                let mut n = NodeSearch::from_spec(n_spec.clone(), node_nr, db).ok_or(
+                    Error::ImpossibleSearch(format!(
+                        "could not create node search for node {} ({})",
+                        node_nr, n_spec
+                    )),
+                )?;
                 node2component.insert(node_nr, node_nr);
 
                 let (orig_query_frag, orig_impl_desc, cost) = if let Some(d) = n.get_desc() {
-                    (d.query_fragment.clone(), d.impl_description.clone(), d.cost.clone())
+                    (
+                        d.query_fragment.clone(),
+                        d.impl_description.clone(),
+                        d.cost.clone(),
+                    )
                 } else {
                     (String::from(""), String::from(""), None)
                 };
@@ -149,20 +156,25 @@ impl<'a> Conjunction<'a> {
         for op_entry in self.operators.drain(..) {
             let component_left = node2component
                 .get(&op_entry.idx_left)
-                .ok_or(Error::ImpossibleSearch(format!("no component for node #{}", op_entry.idx_left+1)))?
+                .ok_or(Error::ImpossibleSearch(format!(
+                    "no component for node #{}",
+                    op_entry.idx_left + 1
+                )))?
                 .clone();
             let component_right = node2component
                 .get(&op_entry.idx_right)
-                .ok_or(Error::ImpossibleSearch(format!("no component for node #{}", op_entry.idx_right+1)))?
+                .ok_or(Error::ImpossibleSearch(format!(
+                    "no component for node #{}",
+                    op_entry.idx_right + 1
+                )))?
                 .clone();
 
-            let exec_left = component2exec
-                .remove(&component_left)
-                .ok_or(Error::ImpossibleSearch(format!("no execution node for component {}", component_left)))?;
-            let exec_right = component2exec
-                .remove(&component_right)
-                .ok_or(Error::ImpossibleSearch(format!("no execution node for component {}", component_right)))?;
-
+            let exec_left = component2exec.remove(&component_left).ok_or(
+                Error::ImpossibleSearch(format!(
+                    "no execution node for component {}",
+                    component_left
+                )),
+            )?;
             let idx_left = exec_left
                 .get_desc()
                 .ok_or(Error::MissingDescription)?
@@ -170,62 +182,79 @@ impl<'a> Conjunction<'a> {
                 .get(&op_entry.idx_left)
                 .ok_or(Error::OperatorIdxNotFound)?
                 .clone();
-            let idx_right = exec_right
-                .get_desc()
-                .ok_or(Error::MissingDescription)?
-                .node_pos
-                .get(&op_entry.idx_right)
-                .ok_or(Error::OperatorIdxNotFound)?
-                .clone();
 
-            let op: Box<Operator> = op_entry
-                .op
-                .create_operator(db)
-                .ok_or(Error::ImpossibleSearch(format!("could not create operator {:?}", op_entry)))?;
+            let op: Box<Operator> = op_entry.op.create_operator(db).ok_or(
+                Error::ImpossibleSearch(format!("could not create operator {:?}", op_entry)),
+            )?;
 
             let new_exec: Box<ExecutionNode<Item = Vec<Match>>> =
                 if component_left == component_right {
                     // don't create new tuples, only filter the existing ones
                     // TODO: check if LHS or RHS is better suited as filter input iterator
+                    let idx_right = exec_left
+                        .get_desc()
+                        .ok_or(Error::MissingDescription)?
+                        .node_pos
+                        .get(&op_entry.idx_right)
+                        .ok_or(Error::OperatorIdxNotFound)?
+                        .clone();
 
                     let filter = BinaryFilter::new(exec_left, idx_left, idx_right, op);
                     Box::new(filter)
-                } else if exec_right.as_nodesearch().is_some() {
-                    // TODO: use cost estimation to check if an IndexJoin is really better
-
-                    // use index join
-                    let join = IndexJoin::new(
-                        exec_left,
-                        idx_left,
-                        op_entry.idx_left + 1,
-                        op_entry.idx_right + 1,
-                        op,
-                        exec_right.as_nodesearch().unwrap().get_node_search_desc(),
-                        &db,
-                        exec_right.get_desc(),
-                    );
-                    Box::new(join)
                 } else {
-                    // use nested loop as "fallback"
+                    let exec_right = component2exec.remove(&component_right).ok_or(
+                        Error::ImpossibleSearch(format!(
+                            "no execution node for component {}",
+                            component_right
+                        )),
+                    )?;
+                    let idx_right = exec_right
+                        .get_desc()
+                        .ok_or(Error::MissingDescription)?
+                        .node_pos
+                        .get(&op_entry.idx_right)
+                        .ok_or(Error::OperatorIdxNotFound)?
+                        .clone();
 
-                    // TODO: check if LHS and RHS should be switched
+                    if exec_right.as_nodesearch().is_some() {
+                        // TODO: use cost estimation to check if an IndexJoin is really better
 
-                    let join = NestedLoop::new(
-                        exec_left,
-                        exec_right,
-                        idx_left,
-                        idx_right,
-                        op_entry.idx_left + 1,
-                        op_entry.idx_right + 1,
-                        op,
-                    );
+                        // use index join
+                        let join = IndexJoin::new(
+                            exec_left,
+                            idx_left,
+                            op_entry.idx_left + 1,
+                            op_entry.idx_right + 1,
+                            op,
+                            exec_right.as_nodesearch().unwrap().get_node_search_desc(),
+                            &db,
+                            exec_right.get_desc(),
+                        );
+                        Box::new(join)
+                    } else {
+                        // use nested loop as "fallback"
 
-                    Box::new(join)
+                        // TODO: check if LHS and RHS should be switched
+
+                        let join = NestedLoop::new(
+                            exec_left,
+                            exec_right,
+                            idx_left,
+                            idx_right,
+                            op_entry.idx_left + 1,
+                            op_entry.idx_right + 1,
+                            op,
+                        );
+
+                        Box::new(join)
+                    }
                 };
 
             let new_component_nr = new_exec
                 .get_desc()
-                .ok_or(Error::ImpossibleSearch(String::from("missing description for execution node")))?
+                .ok_or(Error::ImpossibleSearch(String::from(
+                    "missing description for execution node",
+                )))?
                 .component_nr;
             update_components_for_nodes(&mut node2component, component_left, new_component_nr);
             update_components_for_nodes(&mut node2component, component_right, new_component_nr);
@@ -244,9 +273,13 @@ impl<'a> Conjunction<'a> {
             }
         }
 
-        let first_component_id = first_component_id.ok_or(Error::ImpossibleSearch(String::from("no component in query at all")))?;
+        let first_component_id = first_component_id.ok_or(Error::ImpossibleSearch(String::from(
+            "no component in query at all",
+        )))?;
         return component2exec
             .remove(&first_component_id)
-            .ok_or(Error::ImpossibleSearch(String::from("could not find execution node for query component")));
+            .ok_or(Error::ImpossibleSearch(String::from(
+                "could not find execution node for query component",
+            )));
     }
 }
