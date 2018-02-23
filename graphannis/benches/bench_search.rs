91ebc7eb1391f2b953c67ffec28627ddc4402f3a
Thomas Krause <thomaskrause@posteo.de>
2018-02-23 14:38:52 +0100

    Use lazyly initialized static variable for benchmark

--- graphannis/benches/bench_search.rs
+++ graphannis/benches/bench_search.rs
@@ -1,14 +1,27 @@
 #[macro_use]
 extern crate bencher;
 
+#[macro_use]
+extern crate lazy_static;
+
 extern crate graphannis;
 
 use bencher::Bencher;
 use std::path::PathBuf;
-use std::sync::Arc;
 
 use graphannis::util;
 use graphannis::api::corpusstorage::CorpusStorage;
+
+lazy_static! {
+    static ref CS : CorpusStorage =  {
+        let db_dir = PathBuf::from(if let Ok(path) = std::env::var("ANNIS4_TEST_DATA") {
+            path
+        } else {
+            String::from("../data")
+        });
+        CorpusStorage::new(&db_dir).unwrap()    
+    };
+}
 
 fn get_query_dir() -> PathBuf {
     let query_dir = PathBuf::from(if let Ok(path) = std::env::var("ANNIS4_TEST_QUERIES") {
@@ -19,8 +32,8 @@ fn get_query_dir() -> PathBuf {
     query_dir
 }
 
+
 struct GUM {
-    pub cs : Arc<CorpusStorage>,
     pub def : util::SearchDef,
 }
 
@@ -28,12 +41,11 @@ impl bencher::TDynBenchFn for GUM {
 
     #[allow(unused_must_use)]
     fn run(&self, bench: &mut Bencher) {
-        
-        // plan query to make sure all needed components are in main memory
-        self.cs.plan("GUM", &self.def.json);
+
+        CS.preload("GUM");
 
         bench.iter(|| {
-                if let Ok(count) = self.cs.count("GUM", &self.def.json) {
+                if let Ok(count) = CS.count("GUM", &self.def.json) {
                     assert_eq!(self.def.count, count);
                 } else {
                     assert_eq!(self.def.count, 0);
@@ -46,20 +58,13 @@ pub fn count_gum() -> std::vec::Vec<bencher::TestDescAndFn> {
     use bencher::{TestDescAndFn, TestFn, TestDesc};
     use std::borrow::Cow;
 
-    let db_dir = PathBuf::from(if let Ok(path) = std::env::var("ANNIS4_TEST_DATA") {
-        path
-    } else {
-        String::from("../data")
-    });
+    
     let mut d = get_query_dir();
     d.push("SearchTestGUM");
 
     let queries = util::get_queries_from_folder(&d, true);
 
     let mut benches = std::vec::Vec::new();
-
-    let cs = Arc::new(CorpusStorage::new(&db_dir).unwrap());
-    cs.preload("GUM").unwrap();
 
     for def in queries {
 
@@ -68,7 +73,7 @@ pub fn count_gum() -> std::vec::Vec<bencher::TestDescAndFn> {
                 name: Cow::from(def.name.clone()),
                 ignore: false,
             },
-            testfn: TestFn::DynBenchFn(Box::new(GUM{cs: cs.clone(), def})),
+            testfn: TestFn::DynBenchFn(Box::new(GUM{def})),
         });
     }
 
