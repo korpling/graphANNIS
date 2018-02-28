44492fbc3f31675539d27c3217db3890ba445e63
Thomas Krause <thomaskrause@posteo.de>
2018-02-28 12:30:33 +0100

    Allow to give log file as argument

--- graphannis/benches/bench_search.rs
+++ graphannis/benches/bench_search.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate bencher;
 
 #[macro_use]
@@ -8,6 +7,9 @@ extern crate graphannis;
 
 use bencher::Bencher;
 use std::path::PathBuf;
+
+use bencher::{TestOpts, TDynBenchFn};
+
 
 use graphannis::util;
 use graphannis::api::corpusstorage::CorpusStorage;
@@ -19,7 +21,7 @@ lazy_static! {
         } else {
             String::from("../data")
         });
-        CorpusStorage::new(&db_dir).unwrap()    
+        CorpusStorage::new(&db_dir).unwrap()
     };
 }
 
@@ -32,33 +34,29 @@ fn get_query_dir() -> PathBuf {
     query_dir
 }
 
-
 struct GUM {
-    pub def : util::SearchDef,
+    pub def: util::SearchDef,
 }
 
-impl bencher::TDynBenchFn for GUM {
-
+impl TDynBenchFn for GUM {
     #[allow(unused_must_use)]
     fn run(&self, bench: &mut Bencher) {
-
         CS.preload("GUM");
 
         bench.iter(|| {
-                if let Ok(count) = CS.count("GUM", &self.def.json) {
-                    assert_eq!(self.def.count, count);
-                } else {
-                    assert_eq!(self.def.count, 0);
-                }
+            if let Ok(count) = CS.count("GUM", &self.def.json) {
+                assert_eq!(self.def.count, count);
+            } else {
+                assert_eq!(self.def.count, 0);
+            }
         });
     }
 }
 
 pub fn count_gum() -> std::vec::Vec<bencher::TestDescAndFn> {
-    use bencher::{TestDescAndFn, TestFn, TestDesc};
+    use bencher::{TestDesc, TestDescAndFn, TestFn};
     use std::borrow::Cow;
 
-    
     let mut d = get_query_dir();
     d.push("SearchTestGUM");
 
@@ -67,17 +65,24 @@ pub fn count_gum() -> std::vec::Vec<bencher::TestDescAndFn> {
     let mut benches = std::vec::Vec::new();
 
     for def in queries {
-
         benches.push(TestDescAndFn {
             desc: TestDesc {
                 name: Cow::from(def.name.clone()),
                 ignore: false,
             },
-            testfn: TestFn::DynBenchFn(Box::new(GUM{def})),
+            testfn: TestFn::DynBenchFn(Box::new(GUM { def })),
         });
     }
 
     benches
 }
 
-benchmark_main!(count_gum);
+fn main() {
+    use bencher::run_tests_console;
+    let mut test_opts = TestOpts::default();
+    if let Some(f_arg) = std::env::args().skip(1).next() {
+        test_opts.logfile = Some(PathBuf::from(f_arg));
+    }
+    let benches = count_gum();
+    run_tests_console(&test_opts, benches).unwrap();
+}
