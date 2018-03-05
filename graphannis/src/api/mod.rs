273092e2327ccca17074ccf24f4901e2619ad9ab
Thomas Krause <thomaskrause@posteo.de>
2018-03-05 18:08:50 +0100

    Add a copy of the C++ GraphUpdate class as struct

--- graphannis/src/api/mod.rs
+++ graphannis/src/api/mod.rs
@@ -1,1 +1,2 @@
+pub mod update;
 pub mod corpusstorage;
---
+++ graphannis/src/api/update.rs
@@ -0,0 +1,80 @@
+#[derive(Serialize, Deserialize, Clone)]
+pub enum UpdateEvent {
+    AddNode {
+        node_name: String,
+        node_type: String,
+    },
+    DeleteNode {
+        node_name: String,
+    },
+    AddNodeLabel {
+        node_name: String,
+        anno_ns: String,
+        anno_name: String,
+        anno_value: String,
+    },
+    DeleteNodeLabel {
+        node_name: String,
+        anno_ns: String,
+        anno_name: String,
+    },
+    AddEdge {
+        source_node: String,
+        target_node: String,
+        layer: String,
+        component_type: String,
+        component_name: String,
+    },
+    DeleteEdge {
+        source_node: String,
+        target_node: String,
+        layer: String,
+        component_type: String,
+        component_name: String,
+    },
+    AddEdgeLabel {
+        source_node: String,
+        target_node: String,
+        component_type: String,
+        component_name: String,
+        anno_ns: String,
+        anno_name: String,
+        anno_value: String,
+    },
+    DeleteEdgeLabel {
+        source_node: String,
+        target_node: String,
+        component_type: String,
+        component_name: String,
+        anno_ns: String,
+        anno_name: String,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct GraphUpdate {
+    diffs : Vec<(u64, UpdateEvent)>,
+    last_consistent_change_id : u64,
+}
+
+impl GraphUpdate {
+    pub fn new() -> GraphUpdate {
+        GraphUpdate {
+            diffs: vec![],
+            last_consistent_change_id: 0,
+        }
+    }
+
+    pub fn add_event(&mut self, event : UpdateEvent) {
+        let change_id = self.last_consistent_change_id + (self.diffs.len() as u64)  + 1;
+        self.diffs.push((change_id, event));
+    }
+
+    pub fn is_consistent(&self) -> bool {
+        if self.diffs.is_empty() {
+            return true;
+        } else {
+            return self.last_consistent_change_id == self.diffs[self.diffs.len()-1].0;
+        }
+    }
+}
