c97a6b72841f1949d5c561e6644d9b6a9add042c
Thomas Krause <thomaskrause@posteo.de>
2018-03-29 17:13:23 +0200

    Use parentheses to make sure an "OR" in a regex still captures the whole string

--- graphannis/src/util/mod.rs
+++ graphannis/src/util/mod.rs
@@ -12,9 +12,9 @@ use std;
 
 pub fn regex_full_match(pattern: &str) -> String {
     let mut full_match_pattern = String::new();
-    full_match_pattern.push_str(r"\A");
+    full_match_pattern.push_str(r"\A(");
     full_match_pattern.push_str(pattern);
-    full_match_pattern.push_str(r"\z");
+    full_match_pattern.push_str(r")\z");
 
     full_match_pattern
 }
