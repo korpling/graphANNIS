8e94892e4e382e4c09d84dc37c94a52b6033bc25
Thomas Krause <thomaskrause@posteo.de>
2018-07-23 16:36:10 -0400

    add init and exit functions

--- python-api/graphannis/api.py
+++ python-api/graphannis/api.py
@@ -1,4 +1,16 @@
-from _ffi import ffi;
+from _ffi import ffi
 
 class CorpusStorageManager:
-    pass
+    def __init__(self, db_dir='data/', use_parallel=True):
+        # TODO: make this work on other platforms (Windows/Mac)
+        self.C = ffi.dlopen('libgraphannis_capi.so')
+
+        self.__cs = self.C.annis_cs_new(db_dir.encode('utf-8'), use_parallel)
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.C.annis_cs_free(self.__cs)
+
+    
