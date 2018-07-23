a98fb09c5f8fe50cd84d3871946c7111f4bb6442
Thomas Krause <thomaskrause@posteo.de>
2018-07-23 16:47:36 -0400

    Implement list() function

--- python-api/graphannis/api.py
+++ python-api/graphannis/api.py
@@ -11,6 +11,17 @@ class CorpusStorageManager:
         return self
     
     def __exit__(self, exc_type, exc_value, traceback):
-        self.C.annis_cs_free(self.__cs)
+        self.C.annis_free(self.__cs)
+
+    def list(self):
+        orig = self.C.annis_cs_list(self.__cs)
+        orig_size = int(self.C.annis_vec_str_size(orig))
+
+        copy = []
+        for idx, val in enumerate(range(orig_size)):
+            corpus_name = ffi.string(self.C.annis_vec_str_get(orig, idx))
+            copy.append(corpus_name.decode('utf-8'))
+        return copy
+
 
     
