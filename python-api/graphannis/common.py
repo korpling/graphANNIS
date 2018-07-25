32a521eb29ee15ad963d6287313ca3d54a3f4ba1
Thomas Krause <thomaskrause@posteo.de>
2018-07-25 18:12:07 -0400

    Move the shared library to the package folder

--- python-api/graphannis/common.py
+++ python-api/graphannis/common.py
@@ -2,13 +2,13 @@ import os, os
 from ._ffi import ffi
 
 # TODO: make this work on other platforms (Windows/Mac)
-parent_dir = os.path.normpath(os.path.realpath(__file__) + '/..')
+package_dir = os.path.normpath(os.path.realpath(__file__) + '/..')
 search_dirs = [
-    parent_dir + '/data/linux-x86-64/libgraphannis_capi.so',
-    parent_dir + '/../package/linux-x86-64/libgraphannis_capi.so',
-    parent_dir + '/../../target/release/libgraphannis_capi.so'
+    package_dir + '/linux-x86-64/libgraphannis_capi.so',
+    package_dir + '/../../target/release/libgraphannis_capi.so'
 ]
 
+shared_obj_file = None
 for d in search_dirs:
     d = os.path.normpath(d)
     if os.path.exists(d):
