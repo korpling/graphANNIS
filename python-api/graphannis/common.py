0ffdfd9d16e2248a16418ca3f1f5f210e99d502c
Thomas Krause <thomaskrause@posteo.de>
2018-07-25 15:20:40 -0400

    Use package directory to include pre-compiled shared libraries

--- python-api/graphannis/common.py
+++ python-api/graphannis/common.py
@@ -3,9 +3,20 @@ from ._ffi import ffi
 
 # TODO: make this work on other platforms (Windows/Mac)
 parent_dir = os.path.normpath(os.path.realpath(__file__) + '/..')
-if os.path.exists(parent_dir + '/data/linux-x86-64/libgraphannis_capi.so'):
-    shared_obj_file = parent_dir + '/data/linux-x86-64/libgraphannis_capi.so'
-else:
-    shared_obj_file = os.path.normpath(parent_dir + '/../../target/release/libgraphannis_capi.so')
+search_dirs = [
+    parent_dir + '/data/linux-x86-64/libgraphannis_capi.so',
+    parent_dir + '/../package/linux-x86-64/libgraphannis_capi.so',
+    parent_dir + '/../../target/release/libgraphannis_capi.so'
+]
+
+for d in search_dirs:
+    d = os.path.normpath(d)
+    if os.path.exists(d):
+        shared_obj_file = d
+        break
+
+if shared_obj_file == None:
+    # let the system search for the shared library
+    shared_obj_file = 'libgraphannis_capi.so'    
 
 CAPI = ffi.dlopen(shared_obj_file)
