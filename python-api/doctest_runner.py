8eea0dfb819b7c8b193d8d75ad72b378863a768b
Thomas Krause <thomaskrause@posteo.de>
2018-07-26 11:10:22 -0400

    Add "att" function to add an attribute to a query

--- python-api/doctest_runner.py
+++ python-api/doctest_runner.py
@@ -2,7 +2,8 @@
 
 if __name__ == '__main__':
     import doctest
-    from graphannis import graph, cs
+    from graphannis import graph, cs, query
     import graphannis
     doctest.testmod(graph)
     doctest.testmod(cs)
+    doctest.testmod(query)
--- python-api/graphannis/query.py
+++ python-api/graphannis/query.py
@@ -1,13 +1,44 @@
 import copy
 
+
 class Conjunction:
     def __init__(self):
-        self.q = {'nodes' : [], 'joins': []}
+        self.q = {'nodes': [], 'joins': []}
 
+    def att(self, name, value, match_regex=False, namespace=None):
+        """ Create a new attribute (node) search in the conjunction. The
+        ID of the attribute is automatically created.
 
-    def anno(self, name, value):
+        >>> from graphannis.query import Conjunction
+        >>> q = Conjunction().att('pos', 'NN')
+        """
         obj = copy.copy(self)
 
+        node_anno = {
+            'name': name,
+            'value': value,
+        }
+
+        if namespace == None:
+            node_anno['qname'] = name
+        else:
+            node_anno['namespace'] = namespace
+            node_anno['qname'] = namespace + ':' + name
+
+        if match_regex:
+            node_anno['textMatching'] = 'REGEXP_EQUAL'
+        else:
+            node_anno['textMatching'] = 'EXACT_EQUAL'
+
+        if name == 'tok':
+            node_anno['token'] = True
+        else:
+            node_anno['token'] = False
+
         node_idx = len(obj.q['nodes'])+1
-        obj.q['nodes'].append({'id' : node_idx})
+        obj.q['nodes'].append(
+            {node_idx: {
+            'id': node_idx,
+            'nodeAnnotations': [node_anno]
+            }})
         return obj
