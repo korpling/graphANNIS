6018400854b1365594050f158f3d86a9e28e41ea
Thomas Krause <thomaskrause@posteo.de>
2018-08-11 10:52:31 +0200

    Add a simple lalrprop examply grammar

---
+++ graphannis/src/parser/aql/ast.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub enum Expr {
+    TokenSearch(TextSearch),
+    AnnoSearch(QName, Option<TextSearch>),
+    BinaryOp(String, BinaryOpType, String),
+    Conjunction(Vec<Box<Expr>>),
+    Disjunction(Vec<Box<Expr>>),
+}
+
+#[derive(Debug)]
+pub struct TextSearch(pub String, pub StringMatchType);   
+
+#[derive(Debug)]
+pub struct QName (pub Option<String>, pub String);
+
+#[derive(Debug)]
+pub enum StringMatchType {
+    Exact,
+    Regex,
+}
+
+#[derive(Debug)]
+pub enum BinaryOpType {
+    Dominance,
+    Pointing,
+    Precedence,
+    Overlap,
+    IdenticalCoverage,
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,2 @@
 pub mod jsonqueryparser;
+pub mod aql;
