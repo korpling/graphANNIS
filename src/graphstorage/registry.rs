224d46a2b50dba126e0752570cc3fe18d719ea49
Thomas Krause <thomaskrause@posteo.de>
2018-10-08 20:40:48 +0200

    Formatting

--- src/graphstorage/registry.rs
+++ src/graphstorage/registry.rs
@@ -1,14 +1,12 @@
-use graphstorage::{GraphStorage, GraphStatistic};
 use super::adjacencylist::AdjacencyListStorage;
-use super::prepost::PrePostOrderStorage;
 use super::linear::LinearGraphStorage;
-use std;
-use std::sync::Arc;
-use std::collections::HashMap;
+use super::prepost::PrePostOrderStorage;
 use errors::*;
+use graphstorage::{GraphStatistic, GraphStorage};
 use serde::Deserialize;
-
-
+use std;
+use std::collections::HashMap;
+use std::sync::Arc;
 
 pub struct GSInfo {
     pub id: String,
@@ -21,14 +19,14 @@ lazy_static! {
         let mut m = HashMap::new();
 
         insert_info::<AdjacencyListStorage>(&mut m);
-        
-        insert_info::<PrePostOrderStorage<u64,u64>>(&mut m);
-        insert_info::<PrePostOrderStorage<u64,u32>>(&mut m);
-        insert_info::<PrePostOrderStorage<u64,u8>>(&mut m);
-        insert_info::<PrePostOrderStorage<u32,u32>>(&mut m);
-        insert_info::<PrePostOrderStorage<u32,u8>>(&mut m);
-        insert_info::<PrePostOrderStorage<u16,u32>>(&mut m);
-        insert_info::<PrePostOrderStorage<u16,u8>>(&mut m);
+
+        insert_info::<PrePostOrderStorage<u64, u64>>(&mut m);
+        insert_info::<PrePostOrderStorage<u64, u32>>(&mut m);
+        insert_info::<PrePostOrderStorage<u64, u8>>(&mut m);
+        insert_info::<PrePostOrderStorage<u32, u32>>(&mut m);
+        insert_info::<PrePostOrderStorage<u32, u8>>(&mut m);
+        insert_info::<PrePostOrderStorage<u16, u32>>(&mut m);
+        insert_info::<PrePostOrderStorage<u16, u8>>(&mut m);
 
         insert_info::<LinearGraphStorage<u64>>(&mut m);
         insert_info::<LinearGraphStorage<u32>>(&mut m);
@@ -43,10 +41,7 @@ pub fn create_writeable() -> AdjacencyListStorage {
     AdjacencyListStorage::new()
 }
 
-
-
-pub fn get_optimal_impl_heuristic(stats : &GraphStatistic) -> GSInfo {
-
+pub fn get_optimal_impl_heuristic(stats: &GraphStatistic) -> GSInfo {
     if stats.max_depth <= 1 {
         // if we don't have any deep graph structures an adjencency list is always fasted (and has no overhead)
         return create_info::<AdjacencyListStorage>();
@@ -70,38 +65,38 @@ pub fn get_optimal_impl_heuristic(stats : &GraphStatistic) -> GSInfo {
     return create_info::<AdjacencyListStorage>();;
 }
 
-fn get_prepostorder_by_size(stats : &GraphStatistic) -> GSInfo {
+fn get_prepostorder_by_size(stats: &GraphStatistic) -> GSInfo {
     if stats.rooted_tree {
         // There are exactly two order values per node and there can be only one order value per node
         // in a tree.
         if stats.nodes < (u16::max_value() / 2) as usize {
             if stats.max_depth < u8::max_value() as usize {
-                return create_info::<PrePostOrderStorage<u16,u8>>();
+                return create_info::<PrePostOrderStorage<u16, u8>>();
             } else if stats.max_depth < u32::max_value() as usize {
-                return create_info::<PrePostOrderStorage<u16,u32>>();
+                return create_info::<PrePostOrderStorage<u16, u32>>();
             }
         } else if stats.nodes < (u32::max_value() / 2) as usize {
             if stats.max_depth < u8::max_value() as usize {
-                return create_info::<PrePostOrderStorage<u32,u8>>();
+                return create_info::<PrePostOrderStorage<u32, u8>>();
             } else if stats.max_depth < u32::max_value() as usize {
-                return create_info::<PrePostOrderStorage<u32,u32>>();
+                return create_info::<PrePostOrderStorage<u32, u32>>();
             }
         } else {
             if stats.max_depth < u8::max_value() as usize {
-                return create_info::<PrePostOrderStorage<u64,u8>>();
+                return create_info::<PrePostOrderStorage<u64, u8>>();
             } else if stats.max_depth < u32::max_value() as usize {
-                return create_info::<PrePostOrderStorage<u64,u32>>();
+                return create_info::<PrePostOrderStorage<u64, u32>>();
             }
         }
     } else {
         if stats.max_depth < u8::max_value() as usize {
-            return create_info::<PrePostOrderStorage<u64,u8>>();
+            return create_info::<PrePostOrderStorage<u64, u8>>();
         }
     }
-    return create_info::<PrePostOrderStorage<u64,u64>>();;
+    return create_info::<PrePostOrderStorage<u64, u64>>();;
 }
 
-fn get_linear_by_size(stats : &GraphStatistic) -> GSInfo {
+fn get_linear_by_size(stats: &GraphStatistic) -> GSInfo {
     if stats.max_depth < u8::max_value() as usize {
         return create_info::<LinearGraphStorage<u8>>();
     } else if stats.max_depth < u16::max_value() as usize {
@@ -113,15 +108,18 @@ fn get_linear_by_size(stats : &GraphStatistic) -> GSInfo {
     }
 }
 
-
-fn insert_info<GS: 'static>(registry : &mut HashMap<String, GSInfo>) 
-where for<'de> GS: GraphStorage + Default +  Deserialize<'de> {
+fn insert_info<GS: 'static>(registry: &mut HashMap<String, GSInfo>)
+where
+    for<'de> GS: GraphStorage + Default + Deserialize<'de>,
+{
     let info = create_info::<GS>();
     registry.insert(info.id.clone(), info);
 }
 
-fn create_info<GS: 'static>() -> GSInfo 
-where for<'de> GS: GraphStorage + Default +  Deserialize<'de> {
+fn create_info<GS: 'static>() -> GSInfo
+where
+    for<'de> GS: GraphStorage + Default + Deserialize<'de>,
+{
     // create an instance to get the name
     let instance = GS::default();
 
@@ -132,22 +130,19 @@ where for<'de> GS: GraphStorage + Default +  Deserialize<'de> {
     }
 }
 
-
 pub fn create_from_info(info: &GSInfo) -> Arc<GraphStorage> {
     (info.constructor)()
 }
 
-
-pub fn deserialize(impl_name : &str, input : &mut std::io::Read) -> Result<Arc<GraphStorage>> {
-
-    let info =  REGISTRY.get(impl_name).ok_or(format!("Could not find implementation for graph storage with name '{}'", impl_name))?;
+pub fn deserialize(impl_name: &str, input: &mut std::io::Read) -> Result<Arc<GraphStorage>> {
+    let info = REGISTRY.get(impl_name).ok_or(format!(
+        "Could not find implementation for graph storage with name '{}'",
+        impl_name
+    ))?;
     return (info.deserialize_func)(input);
 }
 
-
-pub fn serialize(data : Arc<GraphStorage>, writer : &mut std::io::Write) -> Result<String> {
+pub fn serialize(data: Arc<GraphStorage>, writer: &mut std::io::Write) -> Result<String> {
     data.serialize_gs(writer)?;
     Ok(data.serialization_id())
 }
-
-
