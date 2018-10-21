bad323b7671f607f621774d8ea726332d4906421
Thomas Krause <thomaskrause@posteo.de>
2018-10-21 21:19:19 +0200

    Map conjunctions in separate function to avoid a clippy warning about it beeing too complex

--- src/annis/db/aql/mod.rs
+++ src/annis/db/aql/mod.rs
@@ -19,6 +19,169 @@ use lalrpop_util::ParseError;
 use std::collections::BTreeMap;
 use std::collections::HashMap;
 
+fn map_conjunction<'a>(c: ast::Conjunction, offsets : &BTreeMap<usize, usize>) -> Result<Conjunction<'a>> {
+    let mut q = Conjunction::new();
+    // collect and sort all node searches according to their start position in the text
+    let mut pos_to_node: BTreeMap<usize, (NodeSearchSpec, Option<String>)> = BTreeMap::default();
+
+    let mut pos_to_endpos: BTreeMap<usize, usize> = BTreeMap::default();
+
+    let mut legacy_meta_search: Vec<(NodeSearchSpec, ast::Pos)> = Vec::new();
+
+    for f in &c {
+        if let ast::Factor::Literal(literal) = f {
+            match literal {
+                ast::Literal::NodeSearch {
+                    spec,
+                    pos,
+                    variable,
+                } => {
+                    if let Some(pos) = pos {
+                        pos_to_node.insert(pos.start, (spec.clone(), variable.clone()));
+                        pos_to_endpos.insert(pos.start, pos.end);
+                    }
+                }
+                ast::Literal::BinaryOp { lhs, rhs, .. } => {
+                    if let ast::Operand::Literal {
+                        spec,
+                        pos,
+                        variable,
+                    } = lhs
+                    {
+                        pos_to_node
+                            .entry(pos.start)
+                            .or_insert_with(|| (spec.as_ref().clone(), variable.clone()));
+                        pos_to_endpos.entry(pos.start).or_insert_with(|| pos.end);
+                    }
+                    if let ast::Operand::Literal {
+                        spec,
+                        pos,
+                        variable,
+                    } = rhs
+                    {
+                        pos_to_node
+                            .entry(pos.start)
+                            .or_insert_with(|| (spec.as_ref().clone(), variable.clone()));
+                        pos_to_endpos.entry(pos.start).or_insert_with(|| pos.end);
+                    }
+                }
+                ast::Literal::LegacyMetaSearch { spec, pos } => {
+                    legacy_meta_search.push((spec.clone(), pos.clone()));
+                }
+            };
+        }
+    }
+
+    // add all nodes specs in order of their start position
+    let mut first_node_pos: Option<String> = None;
+
+    let mut pos_to_node_id: HashMap<usize, String> = HashMap::default();
+    for (start_pos, (node_spec, variable)) in pos_to_node {
+        let variable = variable.as_ref().map(|s| &**s);
+
+        let start = get_line_and_column_for_pos(start_pos, &offsets);
+        let end = if let Some(end_pos) = pos_to_endpos.get(&start_pos) {
+            Some(get_line_and_column_for_pos(*end_pos, &offsets))
+        } else {
+            None
+        };
+
+        let idx = q.add_node_from_query(node_spec, variable, Some(LineColumnRange { start, end }));
+        pos_to_node_id.insert(start_pos, idx.clone());
+        if first_node_pos.is_none() {
+            first_node_pos = Some(idx);
+        }
+    }
+
+    // add all legacy meta searches
+    {
+        let mut first_meta_idx: Option<String> = None;
+        // TODO: add warning to the user not to use this construct anymore
+        for (spec, _pos) in legacy_meta_search {
+            // add an artificial node that describes the document/corpus node
+            let meta_node_idx = q.add_node(spec, None);
+            if let Some(first_meta_idx) = first_meta_idx.clone() {
+                // avoid nested loops by joining additional meta nodes with a "identical node"
+                q.add_operator(
+                    Box::new(IdenticalNodeSpec {}),
+                    &first_meta_idx,
+                    &meta_node_idx,
+                )?;
+            } else if let Some(first_node_pos) = first_node_pos.clone() {
+                first_meta_idx = Some(meta_node_idx.clone());
+                // add a special join to the first node of the query
+                q.add_operator(
+                    Box::new(PartOfSubCorpusSpec {
+                        min_dist: 1,
+                        max_dist: usize::max_value(),
+                    }),
+                    &first_node_pos,
+                    &meta_node_idx,
+                )?;
+                // Also make sure the matched node is actually a document
+                // (the @* could match anything in the hierarchy, including the toplevel corpus)
+                let doc_anno_idx = q.add_node(
+                    NodeSearchSpec::ExactValue {
+                        ns: Some("annis".to_string()),
+                        name: "doc".to_string(),
+                        val: None,
+                        is_meta: true,
+                    },
+                    None,
+                );
+                q.add_operator(
+                    Box::new(IdenticalNodeSpec {}),
+                    &meta_node_idx,
+                    &doc_anno_idx,
+                )?;
+            }
+        }
+    }
+
+    // finally add all operators
+
+    for f in c {
+        if let ast::Factor::Literal(literal) = f {
+            if let ast::Literal::BinaryOp { lhs, op, rhs, pos } = literal {
+                let idx_left = match lhs {
+                    ast::Operand::Literal { spec, pos, .. } => pos_to_node_id
+                        .entry(pos.start)
+                        .or_insert_with(|| q.add_node(spec.as_ref().clone(), None))
+                        .clone(),
+                    ast::Operand::NodeRef(node_ref) => match node_ref {
+                        ast::NodeRef::ID(id) => id.to_string(),
+                        ast::NodeRef::Name(name) => name,
+                    },
+                };
+
+                let idx_right = match rhs {
+                    ast::Operand::Literal { spec, pos, .. } => pos_to_node_id
+                        .entry(pos.start)
+                        .or_insert_with(|| q.add_node(spec.as_ref().clone(), None))
+                        .clone(),
+                    ast::Operand::NodeRef(node_ref) => match node_ref {
+                        ast::NodeRef::ID(id) => id.to_string(),
+                        ast::NodeRef::Name(name) => name,
+                    },
+                };
+
+                let op_pos: Option<LineColumnRange> = if let Some(pos) = pos {
+                    Some(LineColumnRange {
+                        start: get_line_and_column_for_pos(pos.start, &offsets),
+                        end: Some(get_line_and_column_for_pos(pos.end, &offsets)),
+                    })
+                } else {
+                    None
+                };
+
+                q.add_operator_from_query(make_operator_spec(op), &idx_left, &idx_right, op_pos)?;
+            }
+        }
+    }
+
+    Ok(q)
+}
+
 pub fn parse<'a>(query_as_aql: &str) -> Result<Disjunction<'a>> {
     let ast = parser::DisjunctionParser::new().parse(query_as_aql);
     match ast {
@@ -31,179 +194,8 @@ pub fn parse<'a>(query_as_aql: &str) -> Result<Disjunction<'a>> {
             // map all conjunctions and its literals
             let mut alternatives: Vec<Conjunction> = Vec::new();
             for c in ast {
-                let mut q = Conjunction::new();
-                // collect and sort all node searches according to their start position in the text
-                let mut pos_to_node: BTreeMap<
-                    usize,
-                    (NodeSearchSpec, Option<String>),
-                > = BTreeMap::default();
-
-                let mut pos_to_endpos: BTreeMap<usize, usize> = BTreeMap::default();
-
-                let mut legacy_meta_search: Vec<(NodeSearchSpec, ast::Pos)> = Vec::new();
-
-                for f in &c {
-                    if let ast::Factor::Literal(literal) = f {
-                        match literal {
-                            ast::Literal::NodeSearch {
-                                spec,
-                                pos,
-                                variable,
-                            } => {
-                                if let Some(pos) = pos {
-                                    pos_to_node.insert(pos.start, (spec.clone(), variable.clone()));
-                                    pos_to_endpos.insert(pos.start, pos.end);
-                                }
-                            }
-                            ast::Literal::BinaryOp { lhs, rhs, .. } => {
-                                if let ast::Operand::Literal {
-                                    spec,
-                                    pos,
-                                    variable,
-                                } = lhs
-                                {
-                                    pos_to_node.entry(pos.start).or_insert_with(|| {
-                                        (spec.as_ref().clone(), variable.clone())
-                                    });
-                                    pos_to_endpos.entry(pos.start).or_insert_with(|| pos.end);
-                                }
-                                if let ast::Operand::Literal {
-                                    spec,
-                                    pos,
-                                    variable,
-                                } = rhs
-                                {
-                                    pos_to_node.entry(pos.start).or_insert_with(|| {
-                                        (spec.as_ref().clone(), variable.clone())
-                                    });
-                                    pos_to_endpos.entry(pos.start).or_insert_with(|| pos.end);
-                                }
-                            }
-                            ast::Literal::LegacyMetaSearch { spec, pos } => {
-                                legacy_meta_search.push((spec.clone(), pos.clone()));
-                            }
-                        };
-                    }
-                }
-
-                // add all nodes specs in order of their start position
-                let mut first_node_pos: Option<String> = None;
-
-                let mut pos_to_node_id: HashMap<usize, String> = HashMap::default();
-                for (start_pos, (node_spec, variable)) in pos_to_node {
-                    let variable = variable.as_ref().map(|s| &**s);
-
-                    let start = get_line_and_column_for_pos(start_pos, &offsets);
-                    let end = if let Some(end_pos) = pos_to_endpos.get(&start_pos) {
-                        Some(get_line_and_column_for_pos(*end_pos, &offsets))
-                    } else {
-                        None
-                    };
-
-                    let idx = q.add_node_from_query(
-                        node_spec,
-                        variable,
-                        Some(LineColumnRange { start, end }),
-                    );
-                    pos_to_node_id.insert(start_pos, idx.clone());
-                    if first_node_pos.is_none() {
-                        first_node_pos = Some(idx);
-                    }
-                }
-
-                // add all legacy meta searches
-                {
-                    let mut first_meta_idx: Option<String> = None;
-                    // TODO: add warning to the user not to use this construct anymore
-                    for (spec, _pos) in legacy_meta_search {
-                        // add an artificial node that describes the document/corpus node
-                        let meta_node_idx = q.add_node(spec, None);
-                        if let Some(first_meta_idx) = first_meta_idx.clone() {
-                            // avoid nested loops by joining additional meta nodes with a "identical node"
-                            q.add_operator(
-                                Box::new(IdenticalNodeSpec {}),
-                                &first_meta_idx,
-                                &meta_node_idx,
-                            )?;
-                        } else if let Some(first_node_pos) = first_node_pos.clone() {
-                            first_meta_idx = Some(meta_node_idx.clone());
-                            // add a special join to the first node of the query
-                            q.add_operator(
-                                Box::new(PartOfSubCorpusSpec {
-                                    min_dist: 1,
-                                    max_dist: usize::max_value(),
-                                }),
-                                &first_node_pos,
-                                &meta_node_idx,
-                            )?;
-                            // Also make sure the matched node is actually a document
-                            // (the @* could match anything in the hierarchy, including the toplevel corpus)
-                            let doc_anno_idx = q.add_node(
-                                NodeSearchSpec::ExactValue {
-                                    ns: Some("annis".to_string()),
-                                    name: "doc".to_string(),
-                                    val: None,
-                                    is_meta: true,
-                                },
-                                None,
-                            );
-                            q.add_operator(
-                                Box::new(IdenticalNodeSpec {}),
-                                &meta_node_idx,
-                                &doc_anno_idx,
-                            )?;
-                        }
-                    }
-                }
-
-                // finally add all operators
-
-                for f in c {
-                    if let ast::Factor::Literal(literal) = f {
-                        if let ast::Literal::BinaryOp { lhs, op, rhs, pos } = literal {
-                            let idx_left = match lhs {
-                                ast::Operand::Literal { spec, pos, .. } => pos_to_node_id
-                                    .entry(pos.start)
-                                    .or_insert_with(|| q.add_node(spec.as_ref().clone(), None))
-                                    .clone(),
-                                ast::Operand::NodeRef(node_ref) => match node_ref {
-                                    ast::NodeRef::ID(id) => id.to_string(),
-                                    ast::NodeRef::Name(name) => name,
-                                },
-                            };
-
-                            let idx_right = match rhs {
-                                ast::Operand::Literal { spec, pos, .. } => pos_to_node_id
-                                    .entry(pos.start)
-                                    .or_insert_with(|| q.add_node(spec.as_ref().clone(), None))
-                                    .clone(),
-                                ast::Operand::NodeRef(node_ref) => match node_ref {
-                                    ast::NodeRef::ID(id) => id.to_string(),
-                                    ast::NodeRef::Name(name) => name,
-                                },
-                            };
-
-                            let op_pos: Option<LineColumnRange> = if let Some(pos) = pos {
-                                Some(LineColumnRange {
-                                    start: get_line_and_column_for_pos(pos.start, &offsets),
-                                    end: Some(get_line_and_column_for_pos(pos.end, &offsets)),
-                                })
-                            } else {
-                                None
-                            };
-
-                            q.add_operator_from_query(
-                                make_operator_spec(op),
-                                &idx_left,
-                                &idx_right,
-                                op_pos,
-                            )?;
-                        }
-                    }
-                }
-
                 // add the conjunction to the disjunction
-                alternatives.push(q);
+                alternatives.push(map_conjunction(c, &offsets)?);
             }
             Ok(Disjunction::new(alternatives))
         }
