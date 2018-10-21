990312549569f4e28f90f3cd3082ee3086ac0b9c
Thomas Krause <thomaskrause@posteo.de>
2018-10-21 15:16:12 +0200

    We can't use AtomicUsize here and we also need to wait, seems to be a false positive.
    https://rust-lang-nursery.github.io/rust-clippy/v0.0.212/index.html#mutex_atomic

--- src/annis/db/corpusstorage.rs
+++ src/annis/db/corpusstorage.rs
@@ -247,6 +247,7 @@ impl CorpusStorage {
     ) -> Result<CorpusStorage> {
         let query_config = query::Config { use_parallel_joins };
 
+        #[allow(mutex_atomic)]
         let active_background_workers = Arc::new((Mutex::new(0), Condvar::new()));
         let cs = CorpusStorage {
             db_dir: PathBuf::from(db_dir),
@@ -273,6 +274,7 @@ impl CorpusStorage {
         // get the amount of available memory, use a quarter of it per default
         let cache_strategy: CacheStrategy = CacheStrategy::PercentOfFreeMemory(25.0);
 
+        #[allow(mutex_atomic)]
         let active_background_workers = Arc::new((Mutex::new(0), Condvar::new()));
 
         let cs = CorpusStorage {
