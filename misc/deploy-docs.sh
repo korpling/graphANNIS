29da7953cc71754bfaba53f406a101c7094e210f
Thomas Krause <thomaskrause@posteo.de>
2018-11-25 18:07:37 +0100

    Clone gh-pages instead of creating a new branc

--- misc/deploy-docs.sh
+++ misc/deploy-docs.sh
@@ -3,11 +3,14 @@
 # Used by CI to deploy the existing documentation/book/<version> directory to Github Pages
 
 if [ -n "$GITHUB_API_KEY" ]; then
-    cd "$TRAVIS_BUILD_DIR/documentation/book"
-    git init
-    git checkout -b gh-pages
-    git add ${TRAVIS_TAG}
-    git -c user.name='travis' -c user.email='travis' commit -m documentation
-    git push -q https://thomaskrause:$GITHUB_API_KEY@github.com/corpus-tools/graphANNIS-gh-pages gh-pages &>/dev/null
+    cd "$TRAVIS_BUILD_DIR"
+
+    echo "cloning gh-pages"
+    git clone -q  --single-branch gh-pages https://thomaskrause:$GITHUB_API_KEY@github.com/corpus-tools/graphANNIS gh-pages &>/dev/null
+    cp -R documentation/book/* gh-pages/
+    cd gh-pages
+    git add .
+    git -c user.name='travis' -c user.email='travis' commit -m "update documentation"
+    git push -q https://thomaskrause:$GITHUB_API_KEY@github.com/corpus-tools/graphANNIS gh-pages &>/dev/null
     cd "$TRAVIS_BUILD_DIR"
 fi
