3f8d497caa006668aa8bba608fcbeb13ff95854e
Thomas Krause <thomaskrause@posteo.de>
2019-01-07 20:21:23 +0100

    First attempt at custom estimation for the arity operator

--- src/annis/db/aql/operators/arity.rs
+++ src/annis/db/aql/operators/arity.rs
@@ -1,5 +1,6 @@
 use super::RangeSpec;
 use crate::annis::db::graphstorage::GraphStorage;
+use crate::annis::operator::EstimationType;
 use crate::annis::operator::{UnaryOperator, UnaryOperatorSpec};
 use crate::graph::{Component, ComponentType, Match, NodeID};
 use crate::Graph;
@@ -53,26 +54,67 @@ impl std::fmt::Display for ArityOperator {
     }
 }
 
-
 impl UnaryOperator for ArityOperator {
-
     fn filter_match(&self, m: &Match) -> bool {
         let mut children: FxHashSet<NodeID> = FxHashSet::default();
-            for gs in self.graphstorages.iter() {
-                for out in gs.get_outgoing_edges(m.node) {
-                    children.insert(out);
-                }
+        for gs in self.graphstorages.iter() {
+            for out in gs.get_outgoing_edges(m.node) {
+                children.insert(out);
             }
+        }
 
-            let num_children = children.len();
-            if num_children >= self.allowed_range.min_dist() {
-                match self.allowed_range.max_dist() {
-                    std::ops::Bound::Unbounded => true,
-                    std::ops::Bound::Included(max_dist) => num_children <= max_dist,
-                    std::ops::Bound::Excluded(max_dist) => num_children < max_dist,
-                }
-            } else {
-                false
+        let num_children = children.len();
+        if num_children >= self.allowed_range.min_dist() {
+            match self.allowed_range.max_dist() {
+                std::ops::Bound::Unbounded => true,
+                std::ops::Bound::Included(max_dist) => num_children <= max_dist,
+                std::ops::Bound::Excluded(max_dist) => num_children < max_dist,
             }
+        } else {
+            false
+        }
+    }
+
+    fn estimation_type(&self) -> EstimationType {
+        if let RangeSpec::Bound { min_dist, max_dist } = self.allowed_range {
+            let mut min_matches_any = false;
+            let mut gs_with_stats = 0;
+            let mut sum_max_fan_out = 0;
+            for gs in self.graphstorages.iter() {
+                if let Some(stats) = gs.get_statistics() {
+                    gs_with_stats += 1;
+                    sum_max_fan_out += stats.fan_out_99_percentile;
+                    if min_dist <= stats.max_fan_out {
+                        min_matches_any = true;
+                    }
+                }
+            }
+            
+            if min_matches_any {
+                let max_fan_out = (sum_max_fan_out as f64 / gs_with_stats as f64).round() as usize;
+
+                // clip to asssumed maximum
+                let max_dist = std::cmp::min(max_dist, max_fan_out);
+
+                // TODO: we would need a histogram of the distribution of outgoing edges 
+                // for guessing the the number of matching nodes. For now just assume it is much 
+                // more likely to have a larger range instead of a single value
+                let spec_range_len = max_dist - min_dist + 1;
+                let stats_range_len = max_fan_out;
+
+                let sel = spec_range_len as f64 / (stats_range_len as f64);
+                if sel >= 1.0 {
+                    EstimationType::SELECTIVITY(1.0)
+                } else {
+                    EstimationType::SELECTIVITY(sel)
+                }
+             } else {
+                // no graph storages has the minimum required amount of outgoing edges
+                EstimationType::SELECTIVITY(0.0)
+            }
+        } else {
+            // this range spec allows any number of outgoing edges
+            EstimationType::SELECTIVITY(1.0)
+        }
     }
 }
