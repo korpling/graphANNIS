6e09cbe4d1ad9fe3a895ff61837073e1da67e56a
Thomas Krause <thomaskrause@posteo.de>
2019-01-11 16:21:09 +0100

    Implement filter function for IdenticalValue

--- src/annis/db/aql/operators/identical_value.rs
+++ src/annis/db/aql/operators/identical_value.rs
@@ -1,11 +1,20 @@
+use crate::annis::db::AnnoStorage;
 use crate::annis::db::{Graph, Match};
 use crate::annis::operator::*;
-use crate::annis::types::{AnnoKeyID, Component};
+use crate::annis::types::{AnnoKey, AnnoKeyID, Component, NodeID};
 use std;
+use std::sync::Arc;
+
+#[derive(Debug, Clone, PartialOrd, Ord, Hash, PartialEq, Eq)]
+pub enum Type {
+    TokenText,
+    AnnotationValue,
+}
 
 #[derive(Debug, Clone, PartialOrd, Ord, Hash, PartialEq, Eq)]
 pub struct IdenticalValueSpec {
-    pub key : AnnoKeyID
+    pub lhs_type: Type,
+    pub rhs_type: Type,
 }
 
 impl BinaryOperatorSpec for IdenticalValueSpec {
@@ -13,13 +22,23 @@ impl BinaryOperatorSpec for IdenticalValueSpec {
         vec![]
     }
 
-    fn create_operator(&self, _db: &Graph) -> Option<Box<BinaryOperator>> {
-        Some(Box::new(IdenticalValue {}))
+    fn create_operator(&self, db: &Graph) -> Option<Box<BinaryOperator>> {
+        Some(Box::new(IdenticalValue {
+            node_annos: db.node_annos.clone(),
+            lhs_type: self.lhs_type.clone(),
+            rhs_type: self.rhs_type.clone(),
+            tok_key: db.get_token_key(),
+        }))
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct IdenticalValue;
+#[derive(Clone)]
+pub struct IdenticalValue {
+    node_annos: Arc<AnnoStorage<NodeID>>,
+    tok_key: AnnoKey,
+    lhs_type: Type,
+    rhs_type: Type,
+}
 
 impl std::fmt::Display for IdenticalValue {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -36,7 +55,28 @@ impl BinaryOperator for IdenticalValue {
     }
 
     fn filter_match(&self, lhs: &Match, rhs: &Match) -> bool {
-        lhs.node == rhs.node
+        let lhs_val = match self.lhs_type {
+            Type::AnnotationValue => {
+                self.node_annos.get_value_for_item_by_id(&lhs.node, lhs.anno_key)
+            } 
+            Type::TokenText => {
+                self.node_annos.get_value_for_item(&lhs.node, &self.tok_key)
+            }
+        };
+        let rhs_val = match self.rhs_type {
+            Type::AnnotationValue => {
+                self.node_annos.get_value_for_item_by_id(&rhs.node, rhs.anno_key)
+            } 
+            Type::TokenText => {
+                self.node_annos.get_value_for_item(&rhs.node, &self.tok_key)
+            }
+        };
+
+        if let (Some(lhs_val), Some(rhs_val)) = (lhs_val, rhs_val) {
+            lhs_val == rhs_val
+        } else {
+            false
+        }
     }
 
     fn estimation_type(&self) -> EstimationType {
