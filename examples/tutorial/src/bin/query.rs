318c488b6d57d314bed3efe5d76672859f45cbbd
Thomas Krause <thomaskrause@posteo.de>
2019-03-05 17:22:19 +0100

    Add subcorpus_graph(...) function to rust tutorial

--- examples/tutorial/src/bin/query.rs
+++ examples/tutorial/src/bin/query.rs
@@ -1,13 +1,24 @@
-use graphannis::CorpusStorage;
 use graphannis::corpusstorage::{QueryLanguage, ResultOrder};
+use graphannis::CorpusStorage;
 use std::path::PathBuf;
 
 fn main() {
     let cs = CorpusStorage::with_auto_cache_size(&PathBuf::from("data"), true).unwrap();
-    let number_of_matches = cs.count("tutorial", "tok=/.*s.*/", QueryLanguage::AQL).unwrap();
+    let number_of_matches = cs
+        .count("tutorial", "tok=/.*s.*/", QueryLanguage::AQL)
+        .unwrap();
     println!("Number of matches: {}", number_of_matches);
 
-    let matches = cs.find("tutorial", "tok=/.*s.*/", QueryLanguage::AQL, 0, 100, ResultOrder::Normal).unwrap();
+    let matches = cs
+        .find(
+            "tutorial",
+            "tok=/.*s.*/",
+            QueryLanguage::AQL,
+            0,
+            100,
+            ResultOrder::Normal,
+        )
+        .unwrap();
     for i in 0..matches.len() {
         println!("Match {}: {}", i, matches[i]);
     }
