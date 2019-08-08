3da3b7f89ba22eaa2fd39b92c80e0ec6d2567c6c
Thomas Krause <thomaskrause@posteo.de>
2019-08-08 15:14:47 +0200

    Use a separate download script for mdBook

---
+++ misc/download-mdbook.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+MDBOOK_VERSION=${1:-0.3.1}
+
+
+if [[ -x $HOME/.cargo/bin/mdbook ]]; then
+    echo "Checking for mdBook version ${MDBOOK_VERSION}"
+    EXISTING_MDBOOK_VERSION=`$HOME/.cargo/bin/mdbook --version`
+    echo "Exiting: ${EXISTING_MDBOOK_VERSION}"
+    if [ "mdbook v${MDBOOK_VERSION}" != "${EXISTING_MDBOOK_VERSION}" ]; then
+        echo "Installing mdBook version ${MDBOOK_VERSION}"
+
+        if [[ "$TRAVIS_OS_NAME" == "linux" ]];
+        then
+            curl -L -o mdbook.tar.gz https://github.com/rust-lang-nursery/mdBook/releases/download/v${MDBOOK_VERSION}/mdbook-v${MDBOOK_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+            tar -C $HOME/.cargo/bin/ -zxf mdbook.tar.gz
+        elif [[ "$TRAVIS_OS_NAME" == "osx" ]];
+        then
+            curl -L -o mdbook.tar.gz https://github.com/rust-lang-nursery/mdBook/releases/download/v${MDBOOK_VERSION}/mdbook-v${MDBOOK_VERSION}-x86_64-apple-darwin.tar.gz
+            tar -C $HOME/.cargo/bin/ -zxf mdbook.tar.gz
+        elif [[ "$TRAVIS_OS_NAME" == "windows" ]];
+        then
+            curl -L -o mdbook.zip https://github.com/rust-lang-nursery/mdBook/releases/download/v${MDBOOK_VERSION}/mdBook-v${MDBOOK_VERSION}-x86_64-pc-windows-msvc.zip
+            unzip -d $HOME/.cargo/bin/ mdbook.zip
+        else
+            >&2 echo "Unknown value \"${TRAVIS_OS_NAME}\" for environment variable TRAVIS_OS_NAME"
+        	exit 1
+        fi
+    fi
+fi
+
