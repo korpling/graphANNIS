a439f651a7df2233727ab4233200f64c06e4fb57
Thomas Krause <thomaskrause@posteo.de>
2019-10-21 13:22:56 +0200

    Use reference to node annotation storage in non-parallel index join

--- src/annis/db/exec/indexjoin.rs
+++ src/annis/db/exec/indexjoin.rs
@@ -17,7 +17,7 @@ pub struct IndexJoin<'a> {
     op: Box<dyn BinaryOperator + 'a>,
     lhs_idx: usize,
     node_search_desc: Arc<NodeSearchDesc>,
-    node_annos: Arc<dyn AnnotationStorage<NodeID>>,
+    node_annos: &'a dyn AnnotationStorage<NodeID>,
     desc: Desc,
     global_reflexivity: bool,
 }
@@ -36,7 +36,7 @@ impl<'a> IndexJoin<'a> {
         lhs_idx: usize,
         op_entry: BinaryOperatorEntry<'a>,
         node_search_desc: Arc<NodeSearchDesc>,
-        node_annos: Arc<dyn AnnotationStorage<NodeID>>,
+        node_annos: &'a dyn AnnotationStorage<NodeID>,
         rhs_desc: Option<&Desc>,
     ) -> IndexJoin<'a> {
         let lhs_desc = lhs.get_desc().cloned();
@@ -136,7 +136,7 @@ impl<'a> Iterator for IndexJoin<'a> {
                     // check if all filters are true
                     let mut filter_result = true;
                     for f in &self.node_search_desc.cond {
-                        if !(f)(&m_rhs, self.node_annos.as_ref()) {
+                        if !(f)(&m_rhs, self.node_annos) {
                             filter_result = false;
                             break;
                         }
