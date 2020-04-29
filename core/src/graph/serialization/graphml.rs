0ef9553fb10c9f173f5fc2514f435b514d04b69f
Thomas Krause <krauseto@hu-berlin.de>
2020-04-29 17:43:21 +0200

    Use shorter key IDs to save space and allow mixing node and edge annotations with the same qualified name

--- core/src/graph/serialization/graphml.rs
+++ core/src/graph/serialization/graphml.rs
@@ -1,7 +1,7 @@
 use crate::{
     annostorage::ValueSearch,
     graph::{Graph, ANNIS_NS, NODE_TYPE},
-    types::ComponentType,
+    types::{AnnoKey, ComponentType},
     util::join_qname,
 };
 use anyhow::Result;
@@ -9,27 +9,40 @@ use quick_xml::{
     events::{BytesDecl, BytesEnd, BytesStart, BytesText, Event},
     Writer,
 };
+use std::collections::BTreeMap;
 
 fn export_annotation_keys<CT: ComponentType, W: std::io::Write>(
     graph: &Graph<CT>,
     writer: &mut Writer<W>,
-) -> Result<()> {
+) -> Result<BTreeMap<AnnoKey, String>> {
+    let mut key_id_mapping = BTreeMap::new();
+    let mut id_counter = 0;
+
+    // Export node annotations
     for key in graph.get_node_annos().annotation_keys() {
         let mut key_start = BytesStart::borrowed_name("key".as_bytes());
         let qname = join_qname(&key.ns, &key.name);
-        key_start.push_attribute(("id", qname.as_str()));
+
+        let id = key_id_mapping.entry(key).or_insert_with(|| {
+            let result = format!("k{}", id_counter);
+            id_counter += 1;
+            result
+        });
+
+        key_start.push_attribute(("id", id.as_str()));
         key_start.push_attribute(("for", "node"));
         key_start.push_attribute(("attr.name", qname.as_str()));
         key_start.push_attribute(("attr.type", "string"));
 
         writer.write_event(Event::Empty(key_start))?;
     }
-    Ok(())
+    Ok(key_id_mapping)
 }
 
 fn export_nodes<CT: ComponentType, W: std::io::Write>(
     graph: &Graph<CT>,
     writer: &mut Writer<W>,
+    key_id_mapping: &BTreeMap<AnnoKey, String>,
 ) -> Result<()> {
     for m in graph
         .get_node_annos()
@@ -43,8 +56,14 @@ fn export_nodes<CT: ComponentType, W: std::io::Write>(
         for anno in graph.get_node_annos().get_annotations_for_item(&m.node) {
             let mut data_start = BytesStart::borrowed_name(b"data");
 
-            let qname = join_qname(&anno.key.ns, &anno.key.name);
-            data_start.push_attribute(("key", qname.as_str()));
+            let key_id = key_id_mapping.get(&anno.key).ok_or_else(|| {
+                anyhow!(
+                    "Could not find annotation key ID for {:?} when mapping to GraphML",
+                    &anno.key
+                )
+            })?;
+
+            data_start.push_attribute(("key", key_id.as_str()));
             writer.write_event(Event::Start(data_start))?;
             // Add the annotation value as internal text node
             writer.write_event(Event::Text(BytesText::from_plain(anno.val.as_bytes())))?;
@@ -54,6 +73,13 @@ fn export_nodes<CT: ComponentType, W: std::io::Write>(
         writer.write_event(Event::End(BytesEnd::borrowed(b"node")))?;
     }
     Ok(())
+}
+
+fn export_edges<CT: ComponentType, W: std::io::Write>(
+    graph: &Graph<CT>,
+    writer: &mut Writer<W>,
+) -> Result<()> {
+    todo!()
 }
 
 pub fn export<CT: ComponentType, W: std::io::Write>(graph: &Graph<CT>, output: W) -> Result<()> {
@@ -67,7 +93,7 @@ pub fn export<CT: ComponentType, W: std::io::Write>(graph: &Graph<CT>, output: W
     writer.write_event(Event::Start(BytesStart::borrowed_name(b"graphml")))?;
 
     // Define all valid annotation ns/name pairs
-    export_annotation_keys(graph, &mut writer)?;
+    let key_id_mapping = export_annotation_keys(graph, &mut writer)?;
 
     // We are writing a single graph
     let mut graph_start = BytesStart::borrowed_name("graph".as_bytes());
@@ -75,7 +101,11 @@ pub fn export<CT: ComponentType, W: std::io::Write>(graph: &Graph<CT>, output: W
     writer.write_event(Event::Start(graph_start))?;
 
     // Write out all nodes
-    export_nodes(graph, &mut writer)?;
+    export_nodes(graph, &mut writer, &key_id_mapping)?;
+
+    // Write out all edges
+    //export_edges(graph, &mut writer)?;
+
     writer.write_event(Event::End(BytesEnd::borrowed(b"graph")))?;
     writer.write_event(Event::End(BytesEnd::borrowed(b"graphml")))?;
 
@@ -95,6 +125,11 @@ mod tests {
         let mut u = GraphUpdate::new();
         u.add_event(UpdateEvent::AddNode {
             node_name: "n0".to_string(),
+            node_type: "node".to_string(),
+        })
+        .unwrap();
+        u.add_event(UpdateEvent::AddNode {
+            node_name: "n1".to_string(),
             node_type: "node".to_string(),
         })
         .unwrap();
