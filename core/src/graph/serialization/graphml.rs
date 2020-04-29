1a75067b8572e4f4a0950cba72f21645527c5b56
Thomas Krause <krauseto@hu-berlin.de>
2020-04-29 18:29:20 +0200

    Implement writing out the basic edge structure

--- core/src/graph/serialization/graphml.rs
+++ core/src/graph/serialization/graphml.rs
@@ -1,7 +1,7 @@
 use crate::{
     annostorage::ValueSearch,
     graph::{Graph, ANNIS_NS, NODE_TYPE},
-    types::{AnnoKey, ComponentType},
+    types::{AnnoKey, Annotation, ComponentType, Edge},
     util::join_qname,
 };
 use anyhow::Result;
@@ -11,35 +11,84 @@ use quick_xml::{
 };
 use std::collections::BTreeMap;
 
-fn export_annotation_keys<CT: ComponentType, W: std::io::Write>(
+fn write_annotation_keys<CT: ComponentType, W: std::io::Write>(
     graph: &Graph<CT>,
     writer: &mut Writer<W>,
 ) -> Result<BTreeMap<AnnoKey, String>> {
     let mut key_id_mapping = BTreeMap::new();
     let mut id_counter = 0;
 
-    // Export node annotations
+    // Create node annotation keys
     for key in graph.get_node_annos().annotation_keys() {
-        let mut key_start = BytesStart::borrowed_name("key".as_bytes());
-        let qname = join_qname(&key.ns, &key.name);
-
-        let id = key_id_mapping.entry(key).or_insert_with(|| {
-            let result = format!("k{}", id_counter);
+        if !key_id_mapping.contains_key(&key) {
+            let new_id = format!("k{}", id_counter);
             id_counter += 1;
-            result
-        });
 
-        key_start.push_attribute(("id", id.as_str()));
-        key_start.push_attribute(("for", "node"));
-        key_start.push_attribute(("attr.name", qname.as_str()));
-        key_start.push_attribute(("attr.type", "string"));
+            let qname = join_qname(&key.ns, &key.name);
 
-        writer.write_event(Event::Empty(key_start))?;
+            let mut key_start = BytesStart::borrowed_name("key".as_bytes());
+            key_start.push_attribute(("id", new_id.as_str()));
+            key_start.push_attribute(("for", "node"));
+            key_start.push_attribute(("attr.name", qname.as_str()));
+            key_start.push_attribute(("attr.type", "string"));
+
+            writer.write_event(Event::Empty(key_start))?;
+
+            key_id_mapping.insert(key, new_id);
+        }
     }
+
+    // Create edge annotation keys for all components
+    for c in graph.get_all_components(None, None) {
+        if let Some(gs) = graph.get_graphstorage(&c) {
+            for key in gs.get_anno_storage().annotation_keys() {
+                if !key_id_mapping.contains_key(&key) {
+                    let new_id = format!("k{}", id_counter);
+                    id_counter += 1;
+
+                    let qname = join_qname(&key.ns, &key.name);
+
+                    let mut key_start = BytesStart::borrowed_name("key".as_bytes());
+                    key_start.push_attribute(("id", new_id.as_str()));
+                    key_start.push_attribute(("for", "node"));
+                    key_start.push_attribute(("attr.name", qname.as_str()));
+                    key_start.push_attribute(("attr.type", "string"));
+
+                    writer.write_event(Event::Empty(key_start))?;
+
+                    key_id_mapping.insert(key, new_id);
+                }
+            }
+        }
+    }
+
     Ok(key_id_mapping)
 }
 
-fn export_nodes<CT: ComponentType, W: std::io::Write>(
+fn write_data<W: std::io::Write>(
+    anno: Annotation,
+    writer: &mut Writer<W>,
+    key_id_mapping: &BTreeMap<AnnoKey, String>,
+) -> Result<()> {
+    let mut data_start = BytesStart::borrowed_name(b"data");
+
+    let key_id = key_id_mapping.get(&anno.key).ok_or_else(|| {
+        anyhow!(
+            "Could not find annotation key ID for {:?} when mapping to GraphML",
+            &anno.key
+        )
+    })?;
+
+    data_start.push_attribute(("key", key_id.as_str()));
+    writer.write_event(Event::Start(data_start))?;
+    // Add the annotation value as internal text node
+    writer.write_event(Event::Text(BytesText::from_plain(anno.val.as_bytes())))?;
+    writer.write_event(Event::End(BytesEnd::borrowed(b"data")))?;
+
+    Ok(())
+}
+
+fn write_nodes<CT: ComponentType, W: std::io::Write>(
     graph: &Graph<CT>,
     writer: &mut Writer<W>,
     key_id_mapping: &BTreeMap<AnnoKey, String>,
@@ -50,36 +99,70 @@ fn export_nodes<CT: ComponentType, W: std::io::Write>(
     {
         let mut node_start = BytesStart::borrowed_name("node".as_bytes());
 
-        node_start.push_attribute(("id", m.node.to_string().as_str()));
-        writer.write_event(Event::Start(node_start))?;
-        // Write all annotations of the node as "data" element
-        for anno in graph.get_node_annos().get_annotations_for_item(&m.node) {
-            let mut data_start = BytesStart::borrowed_name(b"data");
+        let mut id = m.node.to_string();
+        id.insert(0, 'n');
 
-            let key_id = key_id_mapping.get(&anno.key).ok_or_else(|| {
-                anyhow!(
-                    "Could not find annotation key ID for {:?} when mapping to GraphML",
-                    &anno.key
-                )
-            })?;
-
-            data_start.push_attribute(("key", key_id.as_str()));
-            writer.write_event(Event::Start(data_start))?;
-            // Add the annotation value as internal text node
-            writer.write_event(Event::Text(BytesText::from_plain(anno.val.as_bytes())))?;
-            writer.write_event(Event::End(BytesEnd::borrowed(b"data")))?;
+        node_start.push_attribute(("id", id.as_str()));
+        let node_annotations = graph.get_node_annos().get_annotations_for_item(&m.node);
+        if node_annotations.is_empty() {
+            // Write an empty XML element without child nodes
+            writer.write_event(Event::Empty(node_start))?;
+        } else {
+            writer.write_event(Event::Start(node_start))?;
+            // Write all annotations of the node as "data" element
+            for anno in node_annotations {
+                write_data(anno, writer, key_id_mapping)?;
+            }
+            writer.write_event(Event::End(BytesEnd::borrowed(b"node")))?;
         }
-
-        writer.write_event(Event::End(BytesEnd::borrowed(b"node")))?;
     }
     Ok(())
 }
 
-fn export_edges<CT: ComponentType, W: std::io::Write>(
+fn write_edges<CT: ComponentType, W: std::io::Write>(
     graph: &Graph<CT>,
     writer: &mut Writer<W>,
+    key_id_mapping: &BTreeMap<AnnoKey, String>,
 ) -> Result<()> {
-    todo!()
+    let mut edge_counter = 0;
+    for c in graph.get_all_components(None, None) {
+        if let Some(gs) = graph.get_graphstorage(&c) {
+            for source in gs.source_nodes() {
+                let mut source_id = source.to_string();
+                source_id.insert(0, 'n');
+
+                for target in gs.get_outgoing_edges(source) {
+                    let mut target_id = target.to_string();
+                    target_id.insert(0, 'n');
+
+                    let edge = Edge { source, target };
+
+                    let mut edge_id = edge_counter.to_string();
+                    edge_counter += 1;
+                    edge_id.insert(0, 'e');
+
+                    let mut edge_start = BytesStart::borrowed_name(b"edge");
+                    edge_start.push_attribute(("id", edge_id.as_str()));
+                    edge_start.push_attribute(("source", source_id.as_str()));
+                    edge_start.push_attribute(("target", target_id.as_str()));
+
+                    let edge_annos = gs.get_anno_storage().get_annotations_for_item(&edge);
+                    if edge_annos.is_empty() {
+                        // Write an empty XML element without child nodes
+                        writer.write_event(Event::Empty(edge_start))?;
+                    } else {
+                        writer.write_event(Event::Start(edge_start))?;
+                        // Write all annotations of the edge as "data" element
+                        for anno in edge_annos {
+                            write_data(anno, writer, key_id_mapping)?;
+                        }
+                        writer.write_event(Event::End(BytesEnd::borrowed(b"edge")))?;
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
 }
 
 pub fn export<CT: ComponentType, W: std::io::Write>(graph: &Graph<CT>, output: W) -> Result<()> {
@@ -93,7 +176,7 @@ pub fn export<CT: ComponentType, W: std::io::Write>(graph: &Graph<CT>, output: W
     writer.write_event(Event::Start(BytesStart::borrowed_name(b"graphml")))?;
 
     // Define all valid annotation ns/name pairs
-    let key_id_mapping = export_annotation_keys(graph, &mut writer)?;
+    let key_id_mapping = write_annotation_keys(graph, &mut writer)?;
 
     // We are writing a single graph
     let mut graph_start = BytesStart::borrowed_name("graph".as_bytes());
@@ -101,10 +184,10 @@ pub fn export<CT: ComponentType, W: std::io::Write>(graph: &Graph<CT>, output: W
     writer.write_event(Event::Start(graph_start))?;
 
     // Write out all nodes
-    export_nodes(graph, &mut writer, &key_id_mapping)?;
+    write_nodes(graph, &mut writer, &key_id_mapping)?;
 
     // Write out all edges
-    //export_edges(graph, &mut writer)?;
+    write_edges(graph, &mut writer, &key_id_mapping)?;
 
     writer.write_event(Event::End(BytesEnd::borrowed(b"graph")))?;
     writer.write_event(Event::End(BytesEnd::borrowed(b"graphml")))?;
@@ -124,13 +207,22 @@ mod tests {
         // Create a sample graph using the simple type
         let mut u = GraphUpdate::new();
         u.add_event(UpdateEvent::AddNode {
-            node_name: "n0".to_string(),
+            node_name: "first_node".to_string(),
             node_type: "node".to_string(),
         })
         .unwrap();
         u.add_event(UpdateEvent::AddNode {
-            node_name: "n1".to_string(),
+            node_name: "second_node".to_string(),
             node_type: "node".to_string(),
+        })
+        .unwrap();
+
+        u.add_event(UpdateEvent::AddEdge {
+            source_node: "first_node".to_string(),
+            target_node: "second_node".to_string(),
+            component_type: "Edge".to_string(),
+            layer: "some_ns".to_string(),
+            component_name: "test_component".to_string(),
         })
         .unwrap();
 
