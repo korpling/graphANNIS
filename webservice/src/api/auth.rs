c0228c9323db187f7329e2e181220aa79e19192d
Thomas Krause <krauseto@hu-berlin.de>
2020-06-09 20:35:20 +0200

    Return the token in the body when login was successful

--- webservice/src/api/auth.rs
+++ webservice/src/api/auth.rs
@@ -1,6 +1,10 @@
 use crate::AppState;
-use actix_web::{post, web};
+use actix_web::{error::ErrorInternalServerError, post, web, Error, HttpResponse};
+use hmac::{Hmac, Mac};
+use jwt::SignWithKey;
 use serde::Deserialize;
+use sha2::Sha256;
+use std::collections::BTreeMap;
 
 #[derive(Serialize, Deserialize)]
 pub struct LoginData {
@@ -9,18 +13,34 @@ pub struct LoginData {
 }
 
 #[post("/login")]
-async fn login(login_data: web::Json<LoginData>, state: web::Data<AppState>) -> String {
+async fn login(
+    login_data: web::Json<LoginData>,
+    state: web::Data<AppState>,
+) -> Result<HttpResponse, Error> {
     // Check if user ID is set in configuration
     let provided_user = &login_data.username_or_email;
     if let Some(user) = state.settings.users.get(provided_user) {
         // Add Salt to password, calculate hash and compare against our settings
         let provided_password = &login_data.password;
-        if bcrypt::verify(&provided_password, &user.password).unwrap() {
-            format!("YES, we know you {}.", &provided_user)
-        } else {
-            format!("You are not you!")
+        let verified = bcrypt::verify(&provided_password, &user.password)
+            .map_err(|e| ErrorInternalServerError(e))?;
+        if verified {
+            // Create the JWT token
+            let key: Hmac<Sha256> = Hmac::new_varkey(b"some-secret").unwrap();
+            let mut claims: BTreeMap<_, &str> = BTreeMap::new();
+            claims.insert("sub", provided_user);
+            // Add the corpus groups and adminstrator status as claims
+            let corpus_groups = &user.corpus_groups.join(",");
+            claims.insert("corpus_groups", &corpus_groups);
+            if user.admin {
+                claims.insert("admin", "true");
+            }
+            // Create the actual token
+            let token_str = claims
+                .sign_with_key(&key)
+                .map_err(|e| ErrorInternalServerError(e))?;
+            return Ok(HttpResponse::Ok().body(token_str));
         }
-    } else {
-        format!("Unknown")
     }
+    Ok(HttpResponse::Unauthorized().finish())
 }
