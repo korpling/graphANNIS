0e82ee5694e0c97d8efcdd52f8da5b61482df835
Thomas Krause <krauseto@hu-berlin.de>
2020-06-15 15:09:29 +0200

    put "edge_annotation" to the front of the path

--- webservice/src/main.rs
+++ webservice/src/main.rs
@@ -127,7 +127,7 @@ async fn main() -> Result<()> {
                 web::get().to(api::corpora::list_components),
             )
             .route(
-                "/corpora/{corpus}/{type}/{layer}/{name}/edge_annotations",
+                "/corpora/{corpus}/edge_annotations/{type}/{layer}/{name}/",
                 web::get().to(api::corpora::edge_annotations),
             )
             .route(
