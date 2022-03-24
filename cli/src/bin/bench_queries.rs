5e8dbb56aa3a019a162ddfb7f054613facf47f0a
Thomas Krause <thomaskrause@posteo.de>
2022-03-24 11:52:43 +0100

    Group benchmarks by the corpus name and allow to set the target measurement time

--- cli/src/bin/bench_queries.rs
+++ cli/src/bin/bench_queries.rs
@@ -6,18 +6,19 @@ extern crate graphannis;
 use clap::*;
 use criterion::BenchmarkGroup;
 use criterion::{measurement::Measurement, Criterion};
+use std::collections::BTreeSet;
 use std::path::{Path, PathBuf};
 use std::time::Duration;
 
 use std::sync::Arc;
 
 use graphannis::corpusstorage::{QueryLanguage, SearchQuery};
-use graphannis::util;
+use graphannis::util::{self, SearchDef};
 use graphannis::CorpusStorage;
 
 pub fn create_query_input<M>(
     data_dir: &Path,
-    queries_file: &Path,
+    queries: Vec<SearchDef>,
     use_parallel_joins: bool,
     benchmark_group: &mut BenchmarkGroup<M>,
 ) where
@@ -25,17 +26,12 @@ pub fn create_query_input<M>(
 {
     let cs = Arc::new(CorpusStorage::with_auto_cache_size(data_dir, use_parallel_joins).unwrap());
 
-    let queries = util::get_queries_from_csv(queries_file, true);
     for def in queries.into_iter() {
-        let mut bench_name = def.corpus[0].clone();
-        bench_name.push('/');
-        bench_name.push_str(&def.name);
-
+        let bench_name = def.name.clone();
         let cs = cs.clone();
 
         benchmark_group.bench_function(&bench_name, move |b| {
             for c in def.corpus.iter() {
-                // TODO: preloading all corpora is necessary, but how do we prevent unloading?
                 cs.preload(c).unwrap();
             }
             let cs = cs.clone();
@@ -107,6 +103,10 @@ fn main() {
                 .help("Changes the default size of the sample for this run. [default: 10]"),
         )
         .arg(Arg::with_name("FILTER").required(false))
+        .arg(Arg::with_name("measurement-time")
+        .long("measurement-time")
+        .takes_value(true)
+        .help(&format!("Changes the default measurement time for this run. [default: 5]")))
         .get_matches();
 
     let mut crit: Criterion = Criterion::default().warm_up_time(Duration::from_millis(500));
@@ -122,6 +122,11 @@ fn main() {
         crit = crit.retain_baseline(baseline.to_string());
     }
 
+    if matches.is_present("measurement-time") {
+        let t = value_t_or_exit!(matches.value_of("measurement-time"), u64);
+        crit = crit.measurement_time(Duration::from_secs(t));
+    }
+
     if let Some(filter) = matches.value_of("FILTER") {
         crit = crit.with_filter(String::from(filter))
     }
@@ -131,7 +136,7 @@ fn main() {
     } else {
         PathBuf::from("data")
     };
-    let queries_dir: PathBuf = if let Some(dir) = matches.value_of("queries") {
+    let queries_file: PathBuf = if let Some(dir) = matches.value_of("queries") {
         PathBuf::from(dir)
     } else {
         PathBuf::from("queries")
@@ -140,8 +145,27 @@ fn main() {
     let use_parallel_joins = matches.is_present("parallel");
 
     let mut crit = crit.with_plots().with_output_color(true);
-    let mut group = crit.benchmark_group("queries");
 
-    create_query_input(&data_dir, &queries_dir, use_parallel_joins, &mut group);
-    group.finish();
+    let queries = util::get_queries_from_csv(&queries_file, true);
+    // Create a benchmark group for each corpus
+    let used_corpora: BTreeSet<String> = queries
+        .iter()
+        .filter_map(|def| def.corpus.first().map(|c| c.to_string()))
+        .collect();
+
+    for c in used_corpora {
+        let mut group = crit.benchmark_group(&c);
+        let queries_for_corpus: Vec<_> = queries
+            .iter()
+            .filter(|def| def.corpus.first() == Some(&c))
+            .cloned()
+            .collect();
+        create_query_input(
+            &data_dir,
+            queries_for_corpus,
+            use_parallel_joins,
+            &mut group,
+        );
+        group.finish();
+    }
 }
