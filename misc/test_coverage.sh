15552a19fc9bde7b25bfb810f41c49707af1dee4
Thomas Krause <thomaskrause@posteo.de>
2022-09-05 12:03:00 +0200

    Adapt code coverage script to Rust 1.60
    https://blog.rust-lang.org/2022/04/07/Rust-1.60.0.html#source-based-code-coverage

--- misc/test_coverage.sh
+++ misc/test_coverage.sh
@@ -1,14 +1,22 @@
-# Make sure you installed grcov via cargo first
+# Make sure you installed grcov via cargo first and that the llvm tools are available
+# 
 # cargo install grcov
+# rustup component add llvm-tools-preview
 
 # Set some environment variables needed by grcov
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off"
+export RUSTFLAGS='-Cinstrument-coverage'
+
+mkdir -p target/coverage/
 
 # Run all tests
-cargo +nightly clean
-cargo +nightly test
-cargo +nightly test -- --ignored
+LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
+LLVM_PROFILE_FILE='cargo-ignored-test-%p-%m.profraw' cargo test -- --ignored
 
 # Generate HTML report in target/debug/coverage/index.html
-grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
+grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html/
+# Also generate a lcov file for further processing
+grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/tests.lcov
+
+# Cleanup
+find . -name *.profraw -delete
