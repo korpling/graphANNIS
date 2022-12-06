ea40bd8ac96a34b406825c85c99c487c712fac7c
Thomas Krause <thomaskrause@posteo.de>
2022-12-06 09:32:18 +0100

    Ignore tests and auto-generated files in test coverage report

--- misc/test_coverage.sh
+++ misc/test_coverage.sh
@@ -14,9 +14,9 @@ LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
 LLVM_PROFILE_FILE='cargo-ignored-test-%p-%m.profraw' cargo test -- --ignored
 
 # Generate HTML report in target/debug/coverage/index.html
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html/
+grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '*/tests/*' --ignore 'target/*' --ignore '../*' --ignore "/*" -o target/coverage/html/
 # Also generate a lcov file for further processing
-grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/tests.lcov
+grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '*/tests/*' --ignore 'target/*'  --ignore '../*' --ignore "/*" -o target/coverage/tests.lcov
 
 # Cleanup
 find . -name '*.profraw' -delete
