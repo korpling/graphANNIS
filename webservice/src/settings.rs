abaad1ec9d463dbcea3ab01e107da712ee752d8f
Thomas Krause <thomaskrause@posteo.de>
2023-04-24 17:03:50 +0200

    Update config crate to version 0.13 to avoid issues in future Rust versions

--- webservice/src/settings.rs
+++ webservice/src/settings.rs
@@ -80,19 +80,17 @@ pub struct Settings {
 
 impl Settings {
     pub fn with_file<S: Deref<Target = str>>(config_file: Option<S>) -> Result<Self, ConfigError> {
-        let mut config = config::Config::default();
-
         // Use the included default configuration
-        config.merge(config::File::from_str(
+        let mut config = config::Config::builder().add_source(config::File::from_str(
             include_str!("default-settings.toml",),
             config::FileFormat::Toml,
-        ))?;
+        ));
 
         // TODO: load from default locations
-
         if let Some(config_file) = config_file {
-            config.merge(config::File::new(&config_file, config::FileFormat::Toml))?;
+            config = config.add_source(config::File::new(&config_file, config::FileFormat::Toml));
         }
-        config.try_into()
+        let config = config.build()?;
+        Ok(config.try_deserialize()?)
     }
 }
