8ff06a28950bbe95ccb667de68e4809fbecf541f
Thomas Krause <thomas.krause@hu-berlin.de>
2023-10-31 19:02:43 +0100

    Missing closing quote

--- misc/test_coverage.sh
+++ misc/test_coverage.sh
@@ -1,4 +1,4 @@
 cargo llvm-cov clean --workspace
 cargo llvm-cov --no-report --release
 cargo llvm-cov --no-report --release -- --ignored
-cargo llvm-cov report --ignore-filename-regex '(tests?\.rs)|(capi/.*) --release --open
+cargo llvm-cov report --ignore-filename-regex '(tests?\.rs)|(capi/.*)' --release --open
