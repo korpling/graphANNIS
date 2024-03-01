27a41de883f6a1d1e1de00e4b71f68cf30a92b7d
Thomas Krause <thomas.krause@hu-berlin.de>
2024-03-01 17:45:37 +0100

    Add test for SingleTableIterator

--- core/src/util/disk_collections/tests.rs
+++ core/src/util/disk_collections/tests.rs
@@ -3,6 +3,7 @@ use super::*;
 use fake::faker::name::raw::*;
 use fake::locales::*;
 use fake::Fake;
+use tempfile::NamedTempFile;
 
 #[test]
 fn range() {
@@ -106,6 +107,48 @@ fn range() {
     // Unbound end
     let result: Result<Vec<(u8, bool)>> = table.range(3..).collect();
     assert_eq!(vec![(3, true), (4, true), (5, true)], result.unwrap());
+}
+
+#[test]
+fn single_table_iter() {
+    let mut table: DiskMap<u8, bool> = DiskMap::new(
+        None,
+        EvictionStrategy::MaximumItems(3),
+        DEFAULT_BLOCK_CACHE_CAPACITY,
+        BtreeConfig::default().fixed_key_size(1).fixed_value_size(2),
+    )
+    .unwrap();
+    table.insert(0, true).unwrap();
+    table.insert(1, true).unwrap();
+    table.insert(2, true).unwrap();
+    table.insert(3, true).unwrap();
+    table.insert(4, true).unwrap();
+    table.insert(5, true).unwrap();
+
+    // Serialize to sorted string table
+    let tmp_path = NamedTempFile::new().unwrap();
+    table.write_to(tmp_path.path()).unwrap();
+    // Load as new table and check that iterating over all values works
+    let loaded_table: DiskMap<u8, bool> = DiskMap::new(
+        Some(tmp_path.path()),
+        EvictionStrategy::MaximumItems(5),
+        DEFAULT_BLOCK_CACHE_CAPACITY,
+        BtreeConfig::default().fixed_key_size(1).fixed_value_size(2),
+    )
+    .unwrap();
+    let items: Result<Vec<_>> = loaded_table.iter().unwrap().collect();
+    let items = items.unwrap();
+    assert_eq!(
+        vec![
+            (0, true),
+            (1, true),
+            (2, true),
+            (3, true),
+            (4, true),
+            (5, true)
+        ],
+        items
+    );
 }
 
 #[test]
