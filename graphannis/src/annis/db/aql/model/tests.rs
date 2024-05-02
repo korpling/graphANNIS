f48e9b5a94a1570b39a58efd2da2f339bd572e7a
Thomas Krause <thomas.krause@hu-berlin.de>
2024-05-02 16:08:32 +0200

    Extend statistics test to include segmentations

--- graphannis/src/annis/db/aql/model/tests.rs
+++ graphannis/src/annis/db/aql/model/tests.rs
@@ -1,24 +1,28 @@
-use std::path::PathBuf;
+use std::{fs::File, path::PathBuf};
 
-use crate::{annis::db::aql::model::CorpusSizeStatistics, AnnotationGraph};
+use crate::{
+    annis::db::aql::model::CorpusSizeStatistics, AnnotationGraph
+};
 use assert_matches::assert_matches;
 
 #[test]
 fn global_stats_token_count() {
     let cargo_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let input_file = File::open(&cargo_dir.join("tests/SegmentationWithGaps.graphml")).unwrap();
+    let (graph, _config_str) : (AnnotationGraph, _) = graphannis_core::graph::serialization::graphml::import(
+       input_file,
+        false,
+        |_status| {
+        },
+    ).unwrap();
 
-    let data_dir = cargo_dir.join("tests/data");
-
-    let mut graph = AnnotationGraph::with_default_graphstorages(false).unwrap();
-    graph
-        .load_from(&data_dir.join("sample-memory-based"), true)
-        .unwrap();
-
-    graph.calculate_all_statistics().unwrap();
     assert_eq!(true, graph.global_statistics.is_some());
     let global_stats = graph.global_statistics.unwrap();
     assert_eq!(true, global_stats.all_token_in_order_component);
     assert_matches!(global_stats.corpus_size, 
         CorpusSizeStatistics::Token { base_token_count, segmentation_count } 
-        if base_token_count == 44 && segmentation_count.is_empty());
+        if base_token_count == 16 
+            && segmentation_count.len() == 2
+            && *segmentation_count.get("diplomatic").unwrap() == 11 
+            && *segmentation_count.get("norm").unwrap() == 13);
 }
