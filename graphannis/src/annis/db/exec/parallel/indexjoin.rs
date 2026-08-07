a8b5c87655c4080da1f7aa8bb304d168a77c008f
Thomas Krause <thomas.krause@hu-berlin.de>
2026-08-07 15:43:06 +0200

    Fix clippy issue for returning with ? if Option is None

--- graphannis/src/annis/db/exec/parallel/indexjoin.rs
+++ graphannis/src/annis/db/exec/parallel/indexjoin.rs
@@ -243,28 +243,22 @@ impl Iterator for IndexJoin<'_> {
     fn next(&mut self) -> Option<Self::Item> {
         // lazily initialize
         if self.match_receiver.is_none() {
-            self.match_receiver = if let Some(rhs) = self.next_match_receiver() {
+            self.match_receiver = {
+                let rhs = self.next_match_receiver()?;
                 Some(rhs)
-            } else {
-                return None;
             };
         }
 
         loop {
-            {
-                let match_receiver = self.match_receiver.as_mut()?;
-                if let Ok(result) = match_receiver.recv() {
-                    return Some(result);
-                }
+            let match_receiver = self.match_receiver.as_mut()?;
+            if let Ok(result) = match_receiver.recv() {
+                return Some(result);
             }
 
-            // inner was completed once, get new candidates
-            if let Some(rhs) = self.next_match_receiver() {
-                self.match_receiver = Some(rhs);
-            } else {
-                // no more results to fetch
-                return None;
-            }
+            // inner was completed once, get new candidates or return if no more
+            // results to fetch
+            let rhs = self.next_match_receiver()?;
+            self.match_receiver = Some(rhs);
         }
     }
 }
