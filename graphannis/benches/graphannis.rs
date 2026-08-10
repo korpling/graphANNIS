087cca7ffff9398cb5f07487b20c0482a266703a
Thomas Krause <thomas.krause@hu-berlin.de>
2026-08-10 16:44:38 +0200

    Add copies of the find_ benchmarks but with find_extra
    The current benchmarks suggest there is a about 6% worse performance for find()
    queries and these benchmarks for find_extra() allow to compare the two methods
    and isolate the source of the increased workload.
    
    Since find_extra() and find() have roughly the same execution time, the source
    could be the added usize added as value (which increases the memory stored in the
    temporary BTreeMap) or some issue with refactoring out some of the work to
    closures or sub-functions which might not be inlined.

--- graphannis/benches/graphannis.rs
+++ graphannis/benches/graphannis.rs
@@ -74,6 +74,39 @@ fn find_all_nouns_gum(bench: &mut Criterion) {
     });
 }
 
+fn find_extra_all_nouns_gum(bench: &mut Criterion) {
+    if CORPUS_STORAGE.is_none() {
+        return;
+    }
+
+    let cs = CORPUS_STORAGE.as_ref().unwrap();
+
+    let corpora = cs.list();
+    if let Ok(corpora) = corpora {
+        let corpora: HashSet<String> = corpora.into_iter().map(|c| c.name).collect();
+        // ignore if corpus does not exist
+        if corpora.contains("GUM") {
+            cs.preload("GUM").unwrap();
+        } else {
+            return;
+        }
+    }
+
+    bench.bench_function("find_extra_all_nouns_gum", move |b| {
+        cs.preload("GUM").unwrap();
+        b.iter(|| {
+            let query = SearchQuery {
+                corpus_names: &["GUM"],
+                query: "pos=\"NN\"",
+                query_language: QueryLanguage::AQL,
+                timeout: None,
+            };
+            let f = cs.find_extra(query, 0, None, ResultOrder::Normal);
+            assert!(f.is_ok());
+        })
+    });
+}
+
 fn find_first_ten_nouns_gum(bench: &mut Criterion) {
     if CORPUS_STORAGE.is_none() {
         return;
@@ -102,6 +135,39 @@ fn find_first_ten_nouns_gum(bench: &mut Criterion) {
                 timeout: None,
             };
             let f = cs.find(query, 0, Some(10), ResultOrder::Normal);
+            assert!(f.is_ok());
+        })
+    });
+}
+
+fn find_extra_first_ten_nouns_gum(bench: &mut Criterion) {
+    if CORPUS_STORAGE.is_none() {
+        return;
+    }
+
+    let cs = CORPUS_STORAGE.as_ref().unwrap();
+
+    let corpora = cs.list();
+    if let Ok(corpora) = corpora {
+        let corpora: HashSet<String> = corpora.into_iter().map(|c| c.name).collect();
+        // ignore if corpus does not exist
+        if corpora.contains("GUM") {
+            cs.preload("GUM").unwrap();
+        } else {
+            return;
+        }
+    }
+
+    bench.bench_function("find_extra_first_ten_nouns_gum", move |b| {
+        cs.preload("GUM").unwrap();
+        b.iter(|| {
+            let query = SearchQuery {
+                corpus_names: &["GUM"],
+                query: "pos=\"NN\"",
+                query_language: QueryLanguage::AQL,
+                timeout: None,
+            };
+            let f = cs.find_extra(query, 0, Some(10), ResultOrder::Normal);
             assert!(f.is_ok());
         })
     });
@@ -198,6 +264,39 @@ fn find_first_ten_token_gum(bench: &mut Criterion) {
                 timeout: None,
             };
             let f = cs.find(query, 0, Some(10), ResultOrder::Normal);
+            assert!(f.is_ok());
+        })
+    });
+}
+
+fn find_extra_first_ten_token_gum(bench: &mut Criterion) {
+    if CORPUS_STORAGE.is_none() {
+        return;
+    }
+
+    let cs = CORPUS_STORAGE.as_ref().unwrap();
+
+    let corpora = cs.list();
+    if let Ok(corpora) = corpora {
+        let corpora: HashSet<String> = corpora.into_iter().map(|c| c.name).collect();
+        // ignore if corpus does not exist
+        if corpora.contains("GUM") {
+            cs.preload("GUM").unwrap();
+        } else {
+            return;
+        }
+    }
+
+    bench.bench_function("find_extra_first_ten_token_gum", move |b| {
+        cs.preload("GUM").unwrap();
+        b.iter(|| {
+            let query = SearchQuery {
+                corpus_names: &["GUM"],
+                query: "tok",
+                query_language: QueryLanguage::AQL,
+                timeout: None,
+            };
+            let f = cs.find_extra(query, 0, Some(10), ResultOrder::Normal);
             assert!(f.is_ok());
         })
     });
@@ -333,8 +432,11 @@ fn apply_update_ondisk(bench: &mut Criterion) {
 criterion_group!(name=default; config= Criterion::default().sample_size(50); targets =
     deserialize_gum,
     find_first_ten_token_gum,
-    find_first_ten_nouns_gum, 
+    find_first_ten_nouns_gum,
     find_all_nouns_gum,
+    find_extra_first_ten_nouns_gum,
+    find_extra_first_ten_token_gum,
+    find_extra_all_nouns_gum,
     count_extra_coref_anno, count_extra_node);
 criterion_group!(name=apply_update; config= Criterion::default().sample_size(20); targets =
     apply_update_inmemory,
